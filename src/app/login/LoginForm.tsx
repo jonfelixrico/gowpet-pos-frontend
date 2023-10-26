043b445eb3c9c83498356157c357028abaf2372d
Felix <jonfelixrico@gmail.com>
2023-10-26 22:24:00 +0800

    Adjust LoginForm

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -1,11 +1,28 @@
 'use client'
 
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Button, Flex, FormControl, FormLabel, Input } from '@chakra-ui/react'
 import { Formik, Form, Field, FieldProps } from 'formik'
+
+function FieldTemplate ({
+  fieldName,
+  label
+}: {
+  fieldName: string,
+  label: string
+}) {
+  return <Field name={fieldName}>
+    {({ field, form }: FieldProps) => (
+      <FormControl isInvalid={!!form.errors[fieldName] && !!form.touched[fieldName]}>
+        <FormLabel>{label} </FormLabel>
+        <Input {...field} />
+      </FormControl>
+    )}
+  </Field>
+}
 
 export default function LoginForm() {
   return (<Formik
-    initialValues={{ name: 'Sasuke' }}
+    initialValues={{ username: '', password: '' }}
     onSubmit={(values, actions) => {
       setTimeout(() => {
         alert(JSON.stringify(values, null, 2))
@@ -15,22 +32,17 @@ export default function LoginForm() {
   >
     {(props) => (
       <Form>
-        <Field name='name'>
-          {({ field, form }: FieldProps) => (
-            <FormControl isInvalid={!!form.errors.name && !!form.touched.name}>
-              <FormLabel>First name</FormLabel>
-              <Input {...field} placeholder='name' />
-            </FormControl>
-          )}
-        </Field>
-        <Button
-          mt={4}
-          colorScheme='teal'
-          isLoading={props.isSubmitting}
-          type='submit'
-        >
-          Submit
-        </Button>
+        <Flex direction="column" alignItems="stretch" gap="2">
+          <FieldTemplate fieldName="username" label="Username" />
+          <FieldTemplate fieldName="password" label="Password" />
+
+          <Button
+            isLoading={props.isSubmitting}
+            type='submit'
+          >
+            Submit
+          </Button>
+        </Flex>
       </Form>
     )}
   </Formik>)
