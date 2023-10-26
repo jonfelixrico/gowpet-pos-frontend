7e4200e875c7db9cc25e6cd8f0bdf279e0d149e4
Felix <jonfelixrico@gmail.com>
2023-10-26 22:46:16 +0800

    Apply styles to the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,12 @@
+import { Card, CardBody, Flex, Heading } from "@chakra-ui/react";
 import LoginForm from "./LoginForm";
 
 export default function Login() {
-  return <LoginForm />
+  return <Flex direction="column" align="center" gap="2" justify="center" height="100dvh">
+    <Card>
+      <CardBody>
+        <LoginForm />
+      </CardBody>
+    </Card>
+  </Flex>
 }
