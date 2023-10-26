f5750eca0c3e73071cbd9cb3cf9de9fef41bbf94
Felix <jonfelixrico@gmail.com>
2023-10-26 21:53:04 +0800

    Add comment

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,8 @@
 import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 
+// Followed https://chakra-ui.com/getting-started/nextjs-guide#app-directory-setup
+
 export function Providers({ 
     children 
   }: { 
