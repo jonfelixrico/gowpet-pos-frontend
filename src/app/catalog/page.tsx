13635f28c581e892f43632183a22797e1c0d2f17
Felix <jonfelixrico@gmail.com>
2023-10-28 18:17:52 +0800

    Flesh out the catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,15 @@
-export default function Catalog() {
-  return <div>Placeholder</div>
+import { apiFetch } from '@/utils/resource-api-util'
+
+interface CatalogItem {
+  id: string
+  name: string
+  price: number
+  type: 'SERVICE'
+}
+
+export default async function Catalog() {
+  const response = await apiFetch('/catalog')
+  const data = response.json() as unknown as CatalogItem[]
+
+  return data.map((item) => <div key={item.id}>{JSON.stringify(data)}</div>)
 }
