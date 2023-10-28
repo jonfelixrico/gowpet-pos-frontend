2beae682cb4e74b02bbdb0633f986ac7f1f53a65
Felix <jonfelixrico@gmail.com>
2023-10-28 20:32:11 +0800

    Adjust the catalog page for the container

--- src/app/(main)/catalog/page.tsx
+++ src/app/(main)/catalog/page.tsx
@@ -11,14 +11,7 @@ interface CatalogItem {
 
 function Layout({ children }: { children: ReactNode }) {
   return (
-    <Flex
-      direction="column"
-      width="1000px"
-      marginX="auto"
-      height="100dvh"
-      gap="2"
-      as="main"
-    >
+    <Flex direction="column" gap="2" as="main">
       <Flex justify="end">
         <Button>Create</Button>
       </Flex>
