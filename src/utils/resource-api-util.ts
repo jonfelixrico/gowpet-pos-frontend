d30f4e65d069425a99853f6db90590acdcb65a72
Felix <jonfelixrico@gmail.com>
2023-10-28 18:21:23 +0800

    Adjust apiFetch

--- src/utils/resource-api-util.ts
+++ src/utils/resource-api-util.ts
@@ -2,12 +2,15 @@ import { getAuthToken } from './auth-util'
 import { FetchWrapperOptions, fetchWrapper } from './fetch-utils'
 
 export function apiFetch(
-  input: RequestInfo,
+  input: string,
   init?: RequestInit,
   options?: FetchWrapperOptions
 ) {
+  // TODO take this value from env vars
+  const url = new URL(input, 'http://localhost:3005')
+
   return fetchWrapper(
-    input,
+    url.toString(),
     {
       ...(init ?? {}),
       headers: {
