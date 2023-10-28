7cc0d284d485bb2f2cf1aa7b44aa27c6a0e6f203
Felix <jonfelixrico@gmail.com>
2023-10-28 16:41:07 +0800

    Set up redirection

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,29 @@
-import { Card, CardBody, Flex, Heading } from "@chakra-ui/react";
-import LoginForm from "./LoginForm";
+import { Card, CardBody, Flex } from '@chakra-ui/react'
+import LoginForm from './LoginForm'
+import { cookies } from 'next/headers'
+import { verifyToken } from '@/utils/jwt-utils'
+import { redirect } from 'next/navigation'
 
-export default function Login() {
-  return <Flex direction="column" align="center" gap="2" justify="center" height="100dvh">
-    <Card>
-      <CardBody>
-        <LoginForm />
-      </CardBody>
-    </Card>
-  </Flex>
+export default async function Login() {
+  const cookiesStore = cookies()
+  const tokenCookie = cookiesStore.get('token')?.value
+  if (tokenCookie && (await verifyToken(tokenCookie))) {
+    redirect('/home')
+  }
+
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      gap="2"
+      justify="center"
+      height="100dvh"
+    >
+      <Card>
+        <CardBody>
+          <LoginForm />
+        </CardBody>
+      </Card>
+    </Flex>
+  )
 }
