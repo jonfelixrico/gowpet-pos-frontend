ed001ed0e62efcfe01db6b7870a3f50693d70548
Felix <jonfelixrico@gmail.com>
2023-10-28 22:03:26 +0800

    Use card

--- src/app/(main)/catalog/create/page.tsx
+++ src/app/(main)/catalog/create/page.tsx
@@ -1,10 +1,15 @@
-import { Box, Center } from '@chakra-ui/react'
+import { Card, CardBody, Divider, Flex, Heading } from '@chakra-ui/react'
 import CatalogCreateForm from './CatalogCreateForm'
 
 export default function CatalogCreate() {
   return (
-    <Center height="100%">
-      <CatalogCreateForm />
-    </Center>
+    <Flex height="full" width="full" direction="column" gap={2}>
+      <Heading>Create Item</Heading>
+      <Card>
+        <CardBody>
+          <CatalogCreateForm />
+        </CardBody>
+      </Card>
+    </Flex>
   )
 }
