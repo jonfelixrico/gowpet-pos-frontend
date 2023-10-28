d6b7fca9d5f4eee7f982b4c53100c4516b2f2667
Felix <jonfelixrico@gmail.com>
2023-10-28 21:36:29 +0800

    Use CatalogItemTable

--- src/app/(main)/catalog/page.tsx
+++ src/app/(main)/catalog/page.tsx
@@ -1,3 +1,4 @@
+import CatalogItemTable from '@/components/catalog/CatalogItemTable'
 import { apiFetchData } from '@/server-utils/resource-api-util'
 import {
   Box,
@@ -41,38 +42,12 @@ function Layout({ children }: { children: ReactNode }) {
   )
 }
 
-function ContentItem({ item }: { item: CatalogItem }) {
-  return (
-    <Tr>
-      <Td>{item.name}</Td>
-      <Td isNumeric>{item.price}</Td>
-    </Tr>
-  )
-}
-
 function Content({ items }: { items: CatalogItem[] }) {
   if (!items.length) {
     return <Center flex="1">No items</Center>
   }
 
-  return (
-    <TableContainer flex="1">
-      <Table>
-        <Thead>
-          <Tr>
-            <Th>Name</Th>
-            <Th isNumeric>Price</Th>
-          </Tr>
-        </Thead>
-
-        <Tbody>
-          {items.map((item) => (
-            <ContentItem item={item} key={item.id} />
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  )
+  return <CatalogItemTable items={items} />
 }
 
 export default async function Catalog() {
