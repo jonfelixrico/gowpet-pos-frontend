d050feb2b644eb45ba89421731492621b988614f
Felix <jonfelixrico@gmail.com>
2023-10-28 22:27:01 +0800

    Use the component

--- src/app/(main)/catalog/create/page.tsx
+++ src/app/(main)/catalog/create/page.tsx
@@ -1,10 +1,17 @@
-import { Card, CardBody, Divider, Flex, Heading } from '@chakra-ui/react'
+import { Card, CardBody, Flex, Heading } from '@chakra-ui/react'
 import CatalogCreateForm from './CatalogCreateForm'
+import Link from 'next/link'
+import BackIconButton from './BackIconButton'
 
 export default function CatalogCreate() {
   return (
-    <Flex height="full" width="full" direction="column" gap={2}>
-      <Heading>Create Item</Heading>
+    <Flex height="full" width="full" direction="column" gap="5">
+      <Flex gap="5" align="center">
+        <Link href="/catalog">
+          <BackIconButton />
+        </Link>
+        <Heading>Create Item</Heading>
+      </Flex>
       <Card>
         <CardBody>
           <CatalogCreateForm />
