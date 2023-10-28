8109d22d8d3caa7dfea2eb30a778b58291d8bf66
Felix <jonfelixrico@gmail.com>
2023-10-28 14:16:11 +0800

    Revert "Remove pattern matching"
    This reverts commit f67d3fe696441de0dd3cadf4c5464af8f0497ba7.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,22 @@
 import { verifyToken } from '@/utils/jwt-utils'
 import { NextRequest, NextResponse } from 'next/server'
+import UrlPattern from 'url-pattern'
 import { redirect } from './utils/next-utils'
 
+function buildPatterns(routeStrings: string[]) {
+  return routeStrings.map((route) => new UrlPattern(route))
+}
+const PUBLIC_ROUTES = buildPatterns(['/', '/login'])
+function isRoutePublic(route: string) {
+  return PUBLIC_ROUTES.some((pattern) => pattern.match(route))
+}
+
 export default async function middleware(req: NextRequest) {
+  if (isRoutePublic(req.nextUrl.pathname)) {
+    console.debug('Public route; middleware will proceed')
+    return NextResponse.next()
+  }
+
   const token = req.cookies.get('token')?.value
   if (!token) {
     console.debug('No token found; redirecting')
