7403296458f372ed45ce4263e6b9b51495620f2b
Felix <jonfelixrico@gmail.com>
2023-10-28 13:37:48 +0800

    Create middleware

---
+++ src/app/middleware.ts
@@ -0,0 +1,24 @@
+import { verifyToken } from '@/utils/jwt-utils'
+import { NextRequest, NextResponse } from 'next/server'
+import UrlPattern from 'url-pattern'
+
+function buildPatterns(routeStrings: string[]) {
+  return routeStrings.map((route) => new UrlPattern(route))
+}
+const PUBLIC_ROUTES = buildPatterns(['/', '/login'])
+function isRoutePublic(route: string) {
+  return PUBLIC_ROUTES.some((pattern) => pattern.match(route))
+}
+
+export default async function middleware(req: NextRequest) {
+  if (isRoutePublic(req.nextUrl.pathname)) {
+    return NextResponse.next()
+  }
+
+  const token = req.cookies.get('token')?.value
+  if (!token || !(await verifyToken(token))) {
+    return NextResponse.redirect('/login')
+  }
+
+  return NextResponse.next()
+}
