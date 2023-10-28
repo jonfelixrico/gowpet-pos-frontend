dc8c18afa92c4cbbedc00367d089c61867dcf2b7
Felix <jonfelixrico@gmail.com>
2023-10-28 19:05:00 +0800

    Use 100dvh for layout

--- src/app/catalog/layout.tsx
+++ src/app/catalog/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 
 export default function CatalogLayout(props: { children: ReactNode }) {
   return (
-    <Box width="1000px" marginX="auto">
+    <Box width="1000px" marginX="auto" height="100dvh">
       {props.children}
     </Box>
   )
