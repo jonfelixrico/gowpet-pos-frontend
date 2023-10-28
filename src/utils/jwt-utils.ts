420a8891ff3ac5268b28ccf9a94b96f7a591aa64
Felix <jonfelixrico@gmail.com>
2023-10-28 13:08:42 +0800

    Create JWT utils

---
+++ src/utils/jwt-utils.ts
@@ -0,0 +1,27 @@
+import { verify } from 'jsonwebtoken'
+import { fetchWrapper } from './fetch-utils'
+import pify from 'pify'
+
+async function getPublicKey() {
+  // TODO cache the response
+  const response = await fetchWrapper(
+    'http://localhost:3005/authenticate/publicKey'
+  )
+
+  return await response.text()
+}
+
+export async function verifyToken(token: string) {
+  const key = await getPublicKey()
+
+  await new Promise((resolve, reject) => {
+    verify(token, key, {}, (err) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      resolve(true)
+    })
+  })
+}
