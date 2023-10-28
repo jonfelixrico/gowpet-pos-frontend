95e9ad9390d13c9f200b2755ba3d5016a336d977
Felix <jonfelixrico@gmail.com>
2023-10-28 20:32:03 +0800

    Use container

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
+import { Container } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
-  return children
+  return <Container maxW="container.md">{children}</Container>
 }
