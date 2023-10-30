74d80aaa49513d779928304909059185c1991850
Felix <jonfelixrico@gmail.com>
2023-10-31 00:54:09 +0800

    Create actual test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,5 +1,11 @@
 describe('login', () => {
   it('can log in', () => {
     cy.visit('/login')
+
+    cy.get('[data-cy="username"]').type('root')
+    cy.get('[data-cy="password"]').type('password')
+    cy.get('[data-cy="submit"]').click()
+
+    cy.url().should('not.include', 'login')
   })
 })
