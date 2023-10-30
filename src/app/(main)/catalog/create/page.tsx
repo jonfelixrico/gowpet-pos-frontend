f0a7a52b5a22cf205fd79d48a2dc30a706f68ca0
Felix <jonfelixrico@gmail.com>
2023-10-30 20:00:40 +0800

    Relocate the back button

--- src/app/(main)/catalog/create/page.tsx
+++ src/app/(main)/catalog/create/page.tsx
@@ -1,7 +1,7 @@
 import { Card, CardBody, Flex, Heading } from '@chakra-ui/react'
 import CatalogCreateForm from './CatalogCreateForm'
 import Link from 'next/link'
-import BackIconButton from './BackIconButton'
+import BackIconButton from '../BackIconButton'
 
 export default function CatalogCreate() {
   return (
