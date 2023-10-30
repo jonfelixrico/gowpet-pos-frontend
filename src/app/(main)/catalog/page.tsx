cf81a68fba75df7c086c68ad6198e50809c2f05b
Felix <jonfelixrico@gmail.com>
2023-10-31 01:36:38 +0800

    Make adjustments

--- src/app/(main)/catalog/page.tsx
+++ src/app/(main)/catalog/page.tsx
@@ -1,7 +1,7 @@
 import CatalogItemTable from '@/components/catalog/CatalogItemTable'
 import { CatalogItem } from '@/types/CatalogItem'
 import { apiFetchData } from '@/server-utils/resource-api-util'
-import { Box, Button, Center, Divider, Flex } from '@chakra-ui/react'
+import { Button, Center, Divider, Flex } from '@chakra-ui/react'
 import Link from 'next/link'
 import { ReactNode } from 'react'
 
@@ -12,7 +12,7 @@ function Layout({ children }: { children: ReactNode }) {
     <Flex direction="column" gap="2" as="main" height="100%">
       <Flex justify="end">
         <Link href="/catalog/create">
-          <Button colorScheme="blue" data-cy="createBtn">
+          <Button colorScheme="blue" data-cy="create">
             Create
           </Button>
         </Link>
@@ -28,11 +28,7 @@ function Content({ items }: { items: CatalogItem[] }) {
     return <Center flex="1">No items</Center>
   }
 
-  return (
-    <Box data-cy="itemTable">
-      <CatalogItemTable items={items} />
-    </Box>
-  )
+  return <CatalogItemTable items={items} />
 }
 
 export default async function Catalog() {
