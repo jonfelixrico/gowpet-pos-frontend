a1c87297d565f680e061b75135c59d007403ee2f
Felix <jonfelixrico@gmail.com>
2023-10-31 01:36:08 +0800

    Adjust tests

--- cypress/e2e/catalog.cy.ts
+++ cypress/e2e/catalog.cy.ts
@@ -2,28 +2,30 @@ describe('catalog', () => {
   it('has the critical UI elements', () => {
     cy.visit('/catalog')
 
-    cy.get('[data-cy="createBtn"]').should('exist')
-    cy.get('[data-cy="itemTable"').should('exist')
+    cy.get('[data-cy="create"]').should('exist')
+    cy.get('[data-cy="table"').should('exist')
   })
 
   it('allows leaving catalog creation', () => {
     cy.visit('/catalog/create')
-    cy.get('[data-cy="backBtn"]').click()
+    cy.get('[data-cy="back"]').click()
     cy.url().should('contain', '/catalog')
     cy.url().should('not.contain', '/catalog/create')
   })
 
   it('allows item creation', () => {
     cy.visit('/catalog')
-    cy.get('[data-cy="createBtn"]').click()
+    cy.get('[data-cy="create"]').click()
 
     cy.url().should('contain', 'catalog/create')
-    cy.get('[data-cy="name"]').type('Test Item')
-    cy.get('[data-cy="price"]').type('123.45')
+    cy.get('[data-cy="form"] [data-cy="name"]').type('Test Item')
+    cy.get('[data-cy="form"] [data-cy="price"]').type('123.45')
     cy.get('[data-cy="submit"]').click()
 
     cy.url().should('contain', '/catalog')
     cy.url().should('not.contain', '/catalog/create')
-    cy.get('[data-cy="itemTable"]').get('[data-name="Test Item"]')
+    cy.get('[data-cy="table"] [data-cy="row"] [data-cy="name"]').contains(
+      'Test Item'
+    )
   })
 })
