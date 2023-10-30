689389f943ceb5110de01c22ee62ff97c357c0e5
Felix <jonfelixrico@gmail.com>
2023-10-30 21:39:43 +0800

    Integrate into CreateCatalogForm

--- src/app/(main)/catalog/create/CatalogCreateForm.tsx
+++ src/app/(main)/catalog/create/CatalogCreateForm.tsx
@@ -1,21 +1,13 @@
 'use client'
 
 import { fetchJson } from '@/utils/fetch-utils'
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-} from '@chakra-ui/react'
-import { Field, FieldProps, Form, Formik, FormikHelpers } from 'formik'
+import { Button } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
+import { FormikSubmit } from '@/types/formik'
+import CatalogForm, {
+  CatalogFormFields,
+} from '@/components/catalog/CatalogForm'
 
 interface Payload {
   name: string
@@ -25,10 +17,9 @@ interface Payload {
 export default function CatalogCreateForm() {
   const router = useRouter()
 
-  async function handleSubmit(
-    values: Payload,
-    actions: FormikHelpers<Payload>
-  ) {
+  const handleSubmit: FormikSubmit<CatalogFormFields> = async (
+    values: Payload
+  ) => {
     try {
       await fetchJson('/api/catalog/product', {
         method: 'POST',
@@ -48,58 +39,9 @@ export default function CatalogCreateForm() {
     }
   }
 
-  // TODO i18nize this
   return (
-    <Formik
-      initialValues={{ name: '', price: 0 } as Payload}
-      onSubmit={handleSubmit}
-    >
-      {(props) => (
-        <Form>
-          <Flex direction="column" alignItems="stretch" gap="2">
-            <Field name="name">
-              {({ field, form }: FieldProps) => (
-                <FormControl
-                  isInvalid={!!form.errors.name && !!form.touched.name}
-                >
-                  <FormLabel>Name</FormLabel>
-                  <Input
-                    {...field}
-                    type="text"
-                    placeholder="Type your product name here"
-                  />
-                </FormControl>
-              )}
-            </Field>
-
-            <Field name="price">
-              {({ field, form }: FieldProps) => (
-                <FormControl
-                  isInvalid={!!form.errors.price && !!form.touched.price}
-                >
-                  <FormLabel>Unit Price</FormLabel>
-                  <NumberInput
-                    {...field}
-                    min={0}
-                    precision={2}
-                    onChange={(val) => form.setFieldValue(field.name, val)}
-                  >
-                    <NumberInputField />
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-                </FormControl>
-              )}
-            </Field>
-
-            <Button isLoading={props.isSubmitting} type="submit">
-              Create
-            </Button>
-          </Flex>
-        </Form>
-      )}
-    </Formik>
+    <CatalogForm handleSubmit={handleSubmit}>
+      <Button type="submit">Create</Button>
+    </CatalogForm>
   )
 }
