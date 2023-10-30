4b13dbe28dde56de72bfa1434c756eab4d4bb1d6
Felix <jonfelixrico@gmail.com>
2023-10-31 01:31:38 +0800

    Add failing tests

--- cypress/e2e/catalog.cy.ts
+++ cypress/e2e/catalog.cy.ts
@@ -5,4 +5,25 @@ describe('catalog', () => {
     cy.get('[data-cy="createBtn"]').should('exist')
     cy.get('[data-cy="itemTable"').should('exist')
   })
+
+  it('allows leaving catalog creation', () => {
+    cy.visit('/catalog/create')
+    cy.get('[data-cy="backBtn"]').click()
+    cy.url().should('contain', '/catalog')
+    cy.url().should('not.contain', '/catalog/create')
+  })
+
+  it('allows item creation', () => {
+    cy.visit('/catalog')
+    cy.get('[data-cy="createBtn"]').click()
+
+    cy.url().should('contain', 'catalog/create')
+    cy.get('[data-cy="name"]').type('Test Item')
+    cy.get('[data-cy="price"]').type('123.45')
+    cy.get('[data-cy="submit"]').click()
+
+    cy.url().should('contain', '/catalog')
+    cy.url().should('not.contain', '/catalog/create')
+    cy.get('[data-cy="itemTable"]').get('[data-name="Test Item"]')
+  })
 })
