3afc1f4b1b25cba39c27f80178068cdbd8739d4d
Felix <jonfelixrico@gmail.com>
2023-10-30 21:57:09 +0800

    Integrate

--- src/app/(main)/catalog/[catalogId]/edit/page.tsx
+++ src/app/(main)/catalog/[catalogId]/edit/page.tsx
@@ -1,6 +1,9 @@
 import { CatalogItem } from '@/types/CatalogItem'
 import { apiFetchData } from '@/server-utils/resource-api-util'
 import CatalogEditForm from './CatalogEditForm'
+import { Card, CardBody, Flex, Heading } from '@chakra-ui/react'
+import Link from 'next/link'
+import BackIconButton from '../../BackIconButton'
 
 interface Params {
   catalogId: string
@@ -14,5 +17,21 @@ export default async function CatalogEdit({ params }: { params: Params }) {
     }
   )
 
-  return <CatalogEditForm id={data.id} initialValues={data} />
+  return (
+    <Flex height="full" gap={5} direction="column">
+      <Flex align="center" gap={5}>
+        <Link href={`/catalog/${params.catalogId}`} prefetch={false}>
+          <BackIconButton />
+        </Link>
+
+        <Heading>Edit Item</Heading>
+      </Flex>
+
+      <Card>
+        <CardBody>
+          <CatalogEditForm id={data.id} initialValues={data} />
+        </CardBody>
+      </Card>
+    </Flex>
+  )
 }
