383e5440aea891bfcfb0ab9ff9652e238359ac7c
Felix <jonfelixrico@gmail.com>
2023-10-30 19:58:54 +0800

    Flesh out CatalogDetails

--- src/app/(main)/catalog/[catalogId]/page.tsx
+++ src/app/(main)/catalog/[catalogId]/page.tsx
@@ -1,3 +1,17 @@
-export default function CatalogDetails({ catalogId }: { catalogId: string }) {
-  return <div>{catalogId}</div>
+import { CatalogItem } from '@/models/CatalogItem'
+import { apiFetchData } from '@/server-utils/resource-api-util'
+
+interface Params {
+  catalogId: string
+}
+
+export default async function CatalogDetails({ params }: { params: Params }) {
+  const { data } = await apiFetchData<CatalogItem>(
+    `/catalog/product/${params.catalogId}`,
+    {
+      cache: 'no-cache',
+    }
+  )
+
+  return <div>{JSON.stringify(data)}</div>
 }
