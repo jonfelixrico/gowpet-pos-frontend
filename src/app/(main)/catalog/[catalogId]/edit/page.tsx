10e7337ecf9d436a3b64ac0805d0ef683f9a584f
Felix <jonfelixrico@gmail.com>
2023-10-30 21:27:36 +0800

    Flesh out the edit page

--- src/app/(main)/catalog/[catalogId]/edit/page.tsx
+++ src/app/(main)/catalog/[catalogId]/edit/page.tsx
@@ -1,3 +1,45 @@
-export default function CatalogEdit() {
-  return <div>Test</div>
+import { CatalogItem } from '@/models/CatalogItem'
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import { Button, Card, CardBody, Flex, Heading, Spacer } from '@chakra-ui/react'
+import Link from 'next/link'
+import BackIconButton from '../../BackIconButton'
+
+interface Params {
+  catalogId: string
+}
+
+export default async function CatalogEdit({ params }: { params: Params }) {
+  const { data } = await apiFetchData<CatalogItem>(
+    `/catalog/product/${params.catalogId}`,
+    {
+      cache: 'no-cache',
+    }
+  )
+
+  return (
+    <Flex height="full" gap={5} direction="column">
+      <Flex align="center" gap={5}>
+        <Link href={`/catalog/${params.catalogId}`}>
+          <BackIconButton />
+        </Link>
+        <Heading>Edit Item</Heading>
+      </Flex>
+
+      <Card>
+        <CardBody>
+          <Flex direction="column" gap={2}>
+            <Flex gap={2}>
+              <Heading size="sx">Name:</Heading>
+              {data.name}
+            </Flex>
+
+            <Flex gap={2}>
+              <Heading size="sx">Price:</Heading>
+              {data.price}
+            </Flex>
+          </Flex>
+        </CardBody>
+      </Card>
+    </Flex>
+  )
 }
