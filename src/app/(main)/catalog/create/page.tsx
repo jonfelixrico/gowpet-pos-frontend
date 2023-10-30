7703e8626aa679fc868199816f820d5ae8cfb40b
Felix <jonfelixrico@gmail.com>
2023-10-30 22:49:02 +0800

    Integrate server actions

--- src/app/(main)/catalog/create/page.tsx
+++ src/app/(main)/catalog/create/page.tsx
@@ -2,8 +2,26 @@ import { Card, CardBody, Flex, Heading } from '@chakra-ui/react'
 import CatalogCreateForm from './CatalogCreateForm'
 import Link from 'next/link'
 import BackIconButton from '../BackIconButton'
+import { CatalogFormFields } from '@/components/catalog/CatalogForm'
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import { redirect } from 'next/navigation'
 
 export default function CatalogCreate() {
+  async function create(values: CatalogFormFields) {
+    'use server'
+
+    await apiFetchData('/catalog/product', {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    // TODO implement feature where you can toggle going back to the catalog
+    redirect('/catalog')
+  }
+
   return (
     <Flex height="full" width="full" direction="column" gap="5">
       <Flex gap="5" align="center">
@@ -14,7 +32,7 @@ export default function CatalogCreate() {
       </Flex>
       <Card>
         <CardBody>
-          <CatalogCreateForm />
+          <CatalogCreateForm onSubmit={create} />
         </CardBody>
       </Card>
     </Flex>
