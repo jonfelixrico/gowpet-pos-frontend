1a48115ea9ff4fa4ee2e534af6b73d3dfd134588
Felix <jonfelixrico@gmail.com>
2023-10-30 23:51:05 +0800

    Implement delete modal

--- src/app/(main)/catalog/[catalogId]/CatalogDeleteButton.tsx
+++ src/app/(main)/catalog/[catalogId]/CatalogDeleteButton.tsx
@@ -1,17 +1,58 @@
 'use client'
 
-import { Button } from '@chakra-ui/react'
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
 
 export default function CatalogDeleteButton(props: {
   onDelete: () => Promise<void>
 }) {
-  async function promptDelete() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  function promptDelete() {
+    onOpen()
+  }
+
+  async function confirmDelete() {
+    onClose()
     await props.onDelete()
   }
 
   return (
-    <form action={promptDelete}>
-      <Button type="submit">Delete</Button>
-    </form>
+    <>
+      <form action={promptDelete}>
+        <Button type="submit">Delete</Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Delete Item</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              Are you sure you want to delete this item? This action cannot be
+              undone
+            </ModalBody>
+
+            <ModalFooter gap={2}>
+              <Button colorScheme="red" onClick={confirmDelete}>
+                Yes, delete
+              </Button>
+
+              <Button variant="ghost" onClick={onClose}>
+                No, cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </form>
+    </>
   )
 }
