dd4340b9bdadc6d5af01388ea820c3e0b19ca1d1
Felix <jonfelixrico@gmail.com>
2023-10-31 19:46:50 +0800

    Add failing tests for item update

--- cypress/e2e/catalog.cy.ts
+++ cypress/e2e/catalog.cy.ts
@@ -20,4 +20,37 @@ describe('catalog', () => {
     cy.url().should('not.contain', '/catalog/create')
     cy.get('[data-cy="table"] [data-cy="row"] [data-cy="name"]').contains(name)
   })
+
+  it('supports item update', () => {
+    cy.visit('/catalog')
+    cy.get('[data-cy="showDetails"]').first().click()
+
+    // match /catalog/:id
+    cy.url().contains(/\/catalog\/(\w|\d|-)+$/)
+
+    // check navigation to the edit page
+    cy.get('[data-cy="edit"]').click()
+    cy.url().contains(/\/catalog\/.+\/edit/)
+
+    // do data input
+    const newName = `Test data ${Date.now()}`
+    const newPrice = '2023.11'
+
+    cy.get('[data-cy="name"]').type(newName)
+    cy.get('[data-cy="price"]').type(newPrice)
+    cy.get('[data-cy="submit"]').click()
+
+    cy.url().contains(/\/catalog\/(\w|\d|-)+$/)
+    cy.get('[data-cy="name"]').contains(newName)
+    cy.get('[data-cy="price"]').contains(newPrice)
+
+    let id = ''
+    cy.url().then((url) => {
+      const [_, extractedId] = /\/catalog\/((?:\w|\d|-)+)$/.exec(url) ?? []
+      id = extractedId
+    })
+
+    cy.visit('/catalog')
+    cy.get(`[data-cy="row"][data-catalog-id="${id}"]`).should('exist')
+  })
 })
