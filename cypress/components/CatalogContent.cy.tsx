a807e6d5a865699babe84b33e8f5925575cd0aef
Felix <jonfelixrico@gmail.com>
2023-11-01 01:45:46 +0800

    Adjust CatalogContent

--- cypress/components/CatalogContent.cy.tsx
+++ cypress/components/CatalogContent.cy.tsx
@@ -1,8 +1,14 @@
 import CatalogContent from '@/app/(main)/catalog/CatalogContent'
+import { ChakraProvider } from '@chakra-ui/react'
 
 describe('CatalogContenet', () => {
   it('shows the empty message', () => {
-    cy.mount(<CatalogContent items={[]} />)
+    cy.mount(
+      <ChakraProvider>
+        <CatalogContent items={[]} />
+      </ChakraProvider>
+    )
+
     cy.get('[data-cy="empty"]').should('exist')
     cy.get('[data-cy="table"]').should('not.exist')
   })
