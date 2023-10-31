bbe2fdc679897e24883c66473747b07c315a8db7
Felix <jonfelixrico@gmail.com>
2023-11-01 01:52:25 +0800

    Create more tests

--- cypress/components/CatalogContent.cy.tsx
+++ cypress/components/CatalogContent.cy.tsx
@@ -1,4 +1,5 @@
 import CatalogContent from '@/app/(main)/catalog/CatalogContent'
+import { CatalogItem } from '@/types/CatalogItem'
 import { ChakraProvider } from '@chakra-ui/react'
 
 describe('CatalogContenet', () => {
@@ -11,5 +12,48 @@ describe('CatalogContenet', () => {
 
     cy.get('[data-cy="empty"]').should('exist')
     cy.get('[data-cy="table"]').should('not.exist')
+  })
+
+  const ITEMS: CatalogItem[] = [
+    {
+      id: 'sample 1',
+      name: 'sample 1 name',
+      price: 1234.56,
+      type: 'SERVICE',
+    },
+    {
+      id: 'sample 2',
+      name: 'sample 2 name',
+      price: 789.1,
+      type: 'SERVICE',
+    },
+  ]
+
+  it('shows the table', () => {
+    cy.mount(
+      <ChakraProvider>
+        <CatalogContent items={ITEMS} />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-cy="empty"]').should('not.exist')
+    cy.get('[data-cy="table"]').should('exist')
+  })
+
+  it('renders the rows', () => {
+    cy.mount(
+      <ChakraProvider>
+        <CatalogContent items={ITEMS} />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-cy="row"]').should('have.length', 2)
+    const firstRow = cy.get('[data-cy="row"]').eq(0)
+    firstRow.get('[data-cy="name"]').contains('sample 1 name')
+    firstRow.get('[data-cy="price"]').contains('1234.56')
+
+    const secondRow = cy.get('[data-cy="row"]').eq(1)
+    secondRow.get('[data-cy="name"]').contains('sample 2 name')
+    secondRow.get('[data-cy="price"]').contains('789.1')
   })
 })
