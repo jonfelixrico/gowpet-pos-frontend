b1c041cecd3f8163cd7322bb4f0fa18feabad0eb
Felix <jonfelixrico@gmail.com>
2023-11-05 17:47:00 +0800

    Directly put CatalogContent into the page

--- src/app/(main)/(catalog-list)/catalog/page.tsx
+++ src/app/(main)/(catalog-list)/catalog/page.tsx
@@ -1,10 +1,22 @@
 import { CatalogItem } from '@/types/CatalogItem'
 import { apiFetchData } from '@/server-utils/resource-api-util'
-import CatalogContent from './CatalogContent'
+import CatalogItemTable from './CatalogItemTable'
+import { Center } from '@chakra-ui/react'
 
 export const dynamic = 'force-dynamic'
 
 export default async function Catalog() {
   const { data } = await apiFetchData<CatalogItem[]>('/catalog')
-  return <CatalogContent items={data} height="full" width="full" />
+
+  if (!data?.length) {
+    return (
+      <Center width="full" height="full" data-cy="empty">
+        No items
+      </Center>
+    )
+  }
+
+  return (
+    <CatalogItemTable items={data} height="full" width="full" data-cy="table" />
+  )
 }
