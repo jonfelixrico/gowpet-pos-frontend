a1df6bb6e26b3df70800598c9b6c1107c667c06b
Felix <jonfelixrico@gmail.com>
2023-11-05 15:57:52 +0800

    Create layout

---
+++ src/app/(main)/(catalog-list)/catalog/layout.tsx
@@ -0,0 +1,25 @@
+import { Button, Divider, Flex } from '@chakra-ui/react'
+import Link from 'next/link'
+import { ReactNode } from 'react'
+
+export default function CatalogListLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <Flex direction="column" gap="2" as="main" height="100%">
+      <Flex justify="end">
+        <Link href="/catalog/create">
+          <Button colorScheme="blue" data-cy="create">
+            Create
+          </Button>
+        </Link>
+      </Flex>
+
+      <Divider />
+
+      {children}
+    </Flex>
+  )
+}
