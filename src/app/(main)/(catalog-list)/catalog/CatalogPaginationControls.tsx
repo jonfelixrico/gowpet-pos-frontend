e73f90e62a691284b6ef61446ef9c43aadaa3ae7
Felix <jonfelixrico@gmail.com>
2023-11-05 18:09:26 +0800

    Create controls file

---
+++ src/app/(main)/(catalog-list)/catalog/CatalogPaginationControls.tsx
@@ -0,0 +1,61 @@
+import { Flex, IconButton, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import {
+  PiCaretDoubleLeftBold,
+  PiCaretDoubleRightBold,
+  PiCaretLeftBold,
+  PiCaretRightBold,
+} from 'react-icons/pi'
+
+function PaginationControls({
+  pageCount,
+  pageNo,
+  additionalQuery,
+}: {
+  pageNo: number
+  pageCount: number
+  additionalQuery?: Record<string, string>
+}) {
+  const isOnFirstPage = pageNo === 1
+  const isOnLastPage = pageNo === pageCount
+
+  function buildHref(pageNo: number) {
+    return {
+      path: '/catalog',
+      query: {
+        ...additionalQuery,
+        pageNo,
+      },
+    }
+  }
+
+  return (
+    <Flex gap={3} align="center">
+      <Link href={buildHref(0)}>
+        <IconButton aria-label="First page" isDisabled={isOnFirstPage}>
+          <PiCaretDoubleLeftBold />
+        </IconButton>
+      </Link>
+
+      <Link href={buildHref(pageNo - 1)}>
+        <IconButton aria-label="Prev page" isDisabled={isOnFirstPage}>
+          <PiCaretLeftBold />
+        </IconButton>
+      </Link>
+
+      <Text>{`Page ${pageNo} of ${pageCount}`}</Text>
+
+      <Link href={buildHref(pageNo + 1)}>
+        <IconButton aria-label="Next page" isDisabled={isOnLastPage}>
+          <PiCaretRightBold />
+        </IconButton>
+      </Link>
+
+      <Link href={buildHref(pageCount - 1)}>
+        <IconButton aria-label="Last page" isDisabled={isOnLastPage}>
+          <PiCaretDoubleRightBold />
+        </IconButton>
+      </Link>
+    </Flex>
+  )
+}
