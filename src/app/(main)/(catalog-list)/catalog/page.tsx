82b9950946b9e1bd2bacf67a7fa7ce8f4dd307f5
Felix <jonfelixrico@gmail.com>
2023-11-05 16:09:52 +0800

    Adjust page

--- src/app/(main)/(catalog-list)/catalog/page.tsx
+++ src/app/(main)/(catalog-list)/catalog/page.tsx
@@ -8,19 +8,5 @@ export const dynamic = 'force-dynamic'
 
 export default async function Catalog() {
   const { data } = await apiFetchData<CatalogItem[]>('/catalog')
-  return (
-    <Flex direction="column" gap="2" as="main" height="100%">
-      <Flex justify="end">
-        <Link href="/catalog/create">
-          <Button colorScheme="blue" data-cy="create">
-            Create
-          </Button>
-        </Link>
-      </Flex>
-
-      <Divider />
-
-      <CatalogContent items={data} flex={1} />
-    </Flex>
-  )
+  return <CatalogContent items={data} height="full" />
 }
