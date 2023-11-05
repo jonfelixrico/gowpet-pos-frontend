166a2ef130dd5178e65b7e9fa0e5a4f77668583f
Felix <jonfelixrico@gmail.com>
2023-11-05 17:24:58 +0800

    Adjust CatalogSearchInput

--- src/app/(main)/(catalog-list)/catalog/@toolbar/CatalogSearchInput.tsx
+++ src/app/(main)/(catalog-list)/catalog/@toolbar/CatalogSearchInput.tsx
@@ -6,27 +6,36 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react'
-import { ChangeEventHandler, useState } from 'react'
+import Link from 'next/link'
+import { ChangeEventHandler, useMemo, useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
 
 export default function CatalogSearchInput({
   initialValue,
 }: {
-  initialValue: string
+  initialValue?: string | null
 }) {
   const [value, setValue] = useState(initialValue || '')
   const handleInput: ChangeEventHandler<HTMLInputElement> = (event) => {
     setValue(event.target.value)
   }
 
+  const href = useMemo(() => {
+    const queryParams = new URLSearchParams()
+    queryParams.set('searchTerm', value)
+
+    return `/catalog?${queryParams.toString()}`
+  }, [value])
+
   return (
     <InputGroup>
       <Input value={value} onChange={handleInput} />
       <InputRightElement padding={5}>
-        {/* TODO replace with search button */}
-        <IconButton isRound aria-label="Search" size="sm">
-          <FaSearch />
-        </IconButton>
+        <Link href={href} prefetch={false}>
+          <IconButton isRound aria-label="Search" size="sm">
+            <FaSearch />
+          </IconButton>
+        </Link>
       </InputRightElement>
     </InputGroup>
   )
