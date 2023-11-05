74dd74ab14d71b9b64352ad93fb965aeeef5a1f3
Felix <jonfelixrico@gmail.com>
2023-11-05 16:12:22 +0800

    Adjust imports

--- src/app/(main)/(catalog-list)/catalog/page.tsx
+++ src/app/(main)/(catalog-list)/catalog/page.tsx
@@ -1,7 +1,5 @@
 import { CatalogItem } from '@/types/CatalogItem'
 import { apiFetchData } from '@/server-utils/resource-api-util'
-import { Button, Divider, Flex } from '@chakra-ui/react'
-import Link from 'next/link'
 import CatalogContent from './CatalogContent'
 
 export const dynamic = 'force-dynamic'
