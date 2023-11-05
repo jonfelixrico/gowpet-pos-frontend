9258a2275e13f5f15c98c8f57e90463bb2896217
Felix <jonfelixrico@gmail.com>
2023-11-05 17:05:43 +0800

    Adjust spacer

--- src/app/(main)/(catalog-list)/catalog/layout.tsx
+++ src/app/(main)/(catalog-list)/catalog/layout.tsx
@@ -1,4 +1,4 @@
-import { Button, Divider, Flex } from '@chakra-ui/react'
+import { Button, Divider, Flex, Spacer } from '@chakra-ui/react'
 import Link from 'next/link'
 import { ReactNode } from 'react'
 
@@ -11,8 +11,10 @@ export default function CatalogListLayout({
 }) {
   return (
     <Flex direction="column" gap="2" as="main" height="100%">
-      <Flex gap={2} justify="space-between" align="center">
-        <Flex flex={1}>{toolbar}</Flex>
+      <Flex gap={2} align="center">
+        {toolbar}
+
+        <Spacer />
 
         <Link href="/catalog/create">
           <Button colorScheme="blue" data-cy="create">
