316669fc5086c4534daec93f8ca9313becd9ca6a
Felix <jonfelixrico@gmail.com>
2023-11-05 16:59:08 +0800

    Adjust minor changes to toolbar

--- src/app/(main)/(catalog-list)/catalog/layout.tsx
+++ src/app/(main)/(catalog-list)/catalog/layout.tsx
@@ -7,12 +7,12 @@ export default function CatalogListLayout({
   toolbar,
 }: {
   children: ReactNode
-  toolbar?: ReactNode
+  toolbar: ReactNode
 }) {
   return (
     <Flex direction="column" gap="2" as="main" height="100%">
-      <Flex gap={2} justify="end">
-        {toolbar}
+      <Flex gap={2} justify="space-between" align="center">
+        <Flex flex={1}>{toolbar}</Flex>
 
         <Link href="/catalog/create">
           <Button colorScheme="blue" data-cy="create">
