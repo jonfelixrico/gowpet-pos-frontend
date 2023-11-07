5c6478892c1dd0b037a6e4a79b8c4b2fb2790433
Felix <jonfelixrico@gmail.com>
2023-11-07 23:22:25 +0800

    Integrate into the page

--- src/app/(main)/(billing)/billing/create/page.tsx
+++ src/app/(main)/(billing)/billing/create/page.tsx
@@ -1,5 +1,11 @@
-import { Box } from '@chakra-ui/react'
+import BillingCatalogSearch from '@/components/billing/selection/BillingCatalogSearch'
+import { Flex } from '@chakra-ui/react'
 
 export default function BillingCreatePage() {
-  return <Box>Stub</Box>
+  return (
+    <Flex width="full" height="full">
+      <Flex flex={1}>{/* TODO add content here */}</Flex>
+      <BillingCatalogSearch onAdd={() => {}} flex={1} />
+    </Flex>
+  )
 }
