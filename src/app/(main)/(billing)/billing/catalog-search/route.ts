11b1fb5dd6716960e60168137d6b63f12b9bfeaf
Felix <jonfelixrico@gmail.com>
2023-11-08 01:36:47 +0800

    Add comment

--- src/app/(main)/(billing)/billing/catalog-search/route.ts
+++ src/app/(main)/(billing)/billing/catalog-search/route.ts
@@ -2,6 +2,11 @@ import { apiFetchData } from '@/server-utils/resource-api-util'
 import { CatalogItem } from '@/types/CatalogItem'
 import { NextRequest, NextResponse } from 'next/server'
 
+/*
+ * We can just directly call /api/catalog in the client side.
+ * The advantage doing this over directly calling has is that we don't
+ * have to insert the Authorization header.
+ */
 export async function GET(request: NextRequest) {
   const { data, headers } = await apiFetchData<CatalogItem[]>(
     `/catalog?${request.nextUrl.searchParams.toString()}`
