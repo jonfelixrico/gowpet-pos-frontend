8483bd96191359e355ac0859df07fc87913fe762
Felix <jonfelixrico@gmail.com>
2023-11-08 00:14:05 +0800

    Fetch

--- src/app/(main)/(billing)/billing/create/page.tsx
+++ src/app/(main)/(billing)/billing/create/page.tsx
@@ -1,5 +1,10 @@
+import { apiFetchData } from '@/server-utils/resource-api-util'
 import BillingCreateContent from './BillingCreateContent'
+import { CatalogItem } from '@/types/CatalogItem'
 
-export default function BillingCreatePage() {
+export default async function BillingCreatePage() {
+  const { data, headers } =
+    await apiFetchData<CatalogItem[]>('/catalog?pageNo=0')
+
   return <BillingCreateContent />
 }
