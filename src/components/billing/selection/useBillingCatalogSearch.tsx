09457c78c2bf5cac2d036a1b210667f0328eb159
Felix <jonfelixrico@gmail.com>
2023-11-07 23:29:32 +0800

    Add tokens

--- src/components/billing/selection/useBillingCatalogSearch.tsx
+++ src/components/billing/selection/useBillingCatalogSearch.tsx
@@ -1,18 +1,23 @@
 import { fetchData } from '@/utils/fetch-utils'
 import qs from 'query-string'
 import { CatalogItem } from '@/types/CatalogItem'
+import Cookies from 'js-cookie'
 
 export function useBillingCatalogSearch() {
   async function fetchResults(searchTerm: string, pageNo: number) {
-    const url = qs.stringifyUrl({
-      url: '/api/catalog',
-      query: {
-        searchTerm,
-        pageNo,
-      },
+    const qp = qs.stringify({
+      searchTerm,
+      pageNo,
     })
 
-    const { data, headers } = await fetchData<CatalogItem[]>(url)
+    const { data, headers } = await fetchData<CatalogItem[]>(
+      `/api/catalog?${qp}`,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get('token')}`,
+        },
+      }
+    )
     const pageCount = headers.get('X-Total-Count')
     if (pageCount === null) {
       throw new Error('Data error: page count not incldued')
