fdf010615d1d903b4f7485b5404c4276b0c092bc
Felix <jonfelixrico@gmail.com>
2023-11-07 20:31:09 +0800

    Add tests

--- cypress/components/InputBillingItem.cy.tsx
+++ cypress/components/InputBillingItem.cy.tsx
@@ -19,4 +19,56 @@ describe('InputBillingItem', () => {
     cy.get('[data-cy="name"]').should('contain.text', 'foo')
     cy.get('[data-cy="price"]').should('contain.text', '125')
   })
+
+  it('reacts to delete', () => {
+    const onDeleteSpy = cy.spy()
+    cy.mount(
+      <InputBillingItem
+        onDelete={onDeleteSpy}
+        onQuantityChange={() => {}}
+        item={{
+          catalogId: 'id',
+          name: 'foo',
+          price: 125.0,
+          quantity: 10,
+        }}
+      />
+    )
+
+    cy.get('[data-cy="delete"]')
+      .click()
+      .then(() => {
+        expect(onDeleteSpy).to.have.been.calledOnce
+      })
+  })
+
+  it('can adjust quantity', () => {
+    const onChange = cy.spy()
+    cy.mount(
+      <InputBillingItem
+        onDelete={() => {}}
+        onQuantityChange={onChange}
+        item={{
+          catalogId: 'id',
+          name: 'foo',
+          price: 125.0,
+          quantity: 10,
+        }}
+      />
+    )
+
+    // TODO test typing into the input
+
+    cy.get('[data-cy="increment"]')
+      .click()
+      .then(() => {
+        expect(onChange).to.have.been.calledWith(11)
+      })
+
+    cy.get('[data-cy="decrement"]')
+      .click()
+      .then(() => {
+        expect(onChange).to.have.been.calledWith(9)
+      })
+  })
 })
