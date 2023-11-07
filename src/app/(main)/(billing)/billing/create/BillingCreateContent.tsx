3b7d08ef178f1a7858faa75ba854421a199f2670
Felix <jonfelixrico@gmail.com>
2023-11-08 00:31:56 +0800

    Implement onAdd

--- src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
+++ src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
@@ -2,20 +2,41 @@
 
 import BillingCatalogSearch from '@/components/billing/catalog-search/BillingCatalogSearch'
 import { SearchState } from '@/components/billing/catalog-search/BillingCatalogSearch'
+import { Billing } from '@/types/Billing'
+import { CatalogItem } from '@/types/CatalogItem'
 import { Flex } from '@chakra-ui/react'
+import { produce } from 'immer'
+import { useState } from 'react'
 
 export default function BillingCreateContent({
   initialState,
 }: {
   initialState: SearchState
 }) {
+  const [billing, setBilling] = useState<Billing>({
+    items: [],
+  })
+
+  function onItemAdd({ id, name, price }: CatalogItem) {
+    setBilling((billing) =>
+      produce(billing, ({ items }) => {
+        items.push({
+          catalogId: id,
+          name,
+          price,
+          quantity: 1,
+        })
+      })
+    )
+  }
+
   return (
     <Flex width="full" height="full">
-      <Flex flex={1}>{/* TODO add content here */}</Flex>
+      <Flex flex={1}>{JSON.stringify(billing)}</Flex>
       <BillingCatalogSearch
         initialState={initialState}
         flex={1}
-        onAdd={() => {}}
+        onAdd={onItemAdd}
       />
     </Flex>
   )
