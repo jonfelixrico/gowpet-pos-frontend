478e7e574b98e3010ca7144089456eeced24183d
Felix <jonfelixrico@gmail.com>
2023-11-08 21:05:58 +0800

    Integrate the dialog

--- src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
+++ src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
@@ -4,8 +4,9 @@ import BillingCatalogSearch from '@/components/billing/catalog-search/BillingCat
 import { SearchState } from '@/components/billing/catalog-search/BillingCatalogSearch'
 import InputBilling from '@/components/billing/input/InputBilling'
 import { Billing } from '@/types/Billing'
-import { Box, Button, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, useDisclosure } from '@chakra-ui/react'
 import { useState } from 'react'
+import BillingCreateSearchDialog from './BillingCreateSearchDialog'
 
 export default function BillingCreateContent({
   initialState,
@@ -18,27 +19,38 @@ export default function BillingCreateContent({
     items: [],
   })
 
-  return (
-    <Flex width="full" height="full" gap={2}>
-      <Flex flex={1} direction="column">
-        <Flex flex={1} position="relative">
-          <Box position="absolute" width="full" height="full" overflowY="auto">
-            <InputBilling billing={billing} onChange={setBilling} />
-          </Box>
-        </Flex>
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-        <form action={() => onSave(billing)}>
-          <Button width="full" colorScheme="blue" type="submit">
-            Save
-          </Button>
-        </form>
+  return (
+    <>
+      <Flex width="full" height="full" gap={2}>
+        <Flex flex={1} direction="column">
+          <Flex flex={1} position="relative">
+            <Box
+              position="absolute"
+              width="full"
+              height="full"
+              overflowY="auto"
+            >
+              <InputBilling billing={billing} onChange={setBilling} />
+            </Box>
+          </Flex>
+
+          <form action={() => onSave(billing)}>
+            <Button width="full" colorScheme="blue" type="submit">
+              Save
+            </Button>
+          </form>
+        </Flex>
       </Flex>
-      <BillingCatalogSearch
-        initialState={initialState}
-        flex={1}
-        onBillingChange={setBilling}
-        billing={billing}
-      />
-    </Flex>
+
+      <BillingCreateSearchDialog isOpen={isOpen} onClose={onClose}>
+        <BillingCatalogSearch
+          initialState={initialState}
+          onBillingChange={setBilling}
+          billing={billing}
+        />
+      </BillingCreateSearchDialog>
+    </>
   )
 }
