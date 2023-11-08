1ff90ad57bd6cc66fb365f257cf4f73288e4842a
Felix <jonfelixrico@gmail.com>
2023-11-08 21:42:19 +0800

    Add more functionality

--- src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialog.tsx
+++ src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialog.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,20 +10,42 @@ import {
   ModalOverlay,
   ModalProps,
 } from '@chakra-ui/react'
+import { SearchState, useSearch } from './useSearch'
+import BillingCatalogSearchItem from './BillingCatalogSearchItem'
+import { CatalogItem } from '@/types/CatalogItem'
+
+type BillingCatalogSearchDialogProps = {
+  initialState: SearchState
+  onAdd: (item: CatalogItem) => void
+  cannotAdd: Set<string>
+} & Pick<ModalProps, 'isOpen' | 'onClose'>
 
 export default function BillingCreateSearchDialog({
   isOpen,
   onClose,
-  children,
-  ...props
-}: ModalProps) {
+  initialState,
+  onAdd,
+  cannotAdd,
+}: BillingCatalogSearchDialogProps) {
+  const { items, canLoadMore, startSearch, loadMore } = useSearch(initialState)
   return (
-    <Modal {...props} isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Add Item</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>{children}</ModalBody>
+        <ModalBody>
+          <Flex direction="column" gap={3}>
+            {items.map((item) => (
+              <BillingCatalogSearchItem
+                catalogItem={item}
+                onAdd={() => onAdd(item)}
+                key={item.id}
+                canAdd={!cannotAdd.has(item.id)}
+              />
+            ))}
+          </Flex>
+        </ModalBody>
 
         <ModalFooter>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
