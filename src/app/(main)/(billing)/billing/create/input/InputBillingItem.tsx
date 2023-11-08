e7a7c95224a8d4c469b18594b85116f2eb759898
Felix <jonfelixrico@gmail.com>
2023-11-08 22:15:55 +0800

    Adjust the look

--- src/app/(main)/(billing)/billing/create/input/InputBillingItem.tsx
+++ src/app/(main)/(billing)/billing/create/input/InputBillingItem.tsx
@@ -4,6 +4,7 @@ import { Billing } from '@/types/Billing'
 import {
   Box,
   Button,
+  Flex,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -27,22 +28,28 @@ export default function InputBillingItem({
     // TODO stub for now
     <Box>
       <Text data-cy="name">{item.name}</Text>
-      <Text data-cy="price">{item.price}</Text>
-      <Text data-cy="amount">{item.price * item.quantity}</Text>
-      <NumberInput
-        value={item.quantity}
-        onChange={(_, val) => onQuantityChange(val)}
-      >
-        <NumberInputField data-cy="quantity" />
-        <NumberInputStepper>
-          <NumberIncrementStepper data-cy="increment" />
-          <NumberDecrementStepper data-cy="decrement" />
-        </NumberInputStepper>
-      </NumberInput>
+      <Text data-cy="price" fontSize="small">
+        {item.price} per unit
+      </Text>
+      <Text data-cy="amount" fontSize="small">
+        {item.price * item.quantity} for {item.quantity} units
+      </Text>
+      <Flex gap={2}>
+        <NumberInput
+          value={item.quantity}
+          onChange={(_, val) => onQuantityChange(val)}
+        >
+          <NumberInputField data-cy="quantity" />
+          <NumberInputStepper>
+            <NumberIncrementStepper data-cy="increment" />
+            <NumberDecrementStepper data-cy="decrement" />
+          </NumberInputStepper>
+        </NumberInput>
 
-      <Button onClick={onDelete} data-cy="delete">
-        Delete
-      </Button>
+        <Button onClick={onDelete} data-cy="delete">
+          Delete
+        </Button>
+      </Flex>
     </Box>
   )
 }
