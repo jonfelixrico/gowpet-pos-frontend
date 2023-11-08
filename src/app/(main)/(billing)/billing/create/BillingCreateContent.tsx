3e3a3cff03d8b17442b4540c133bf84fc4eafbae
Felix <jonfelixrico@gmail.com>
2023-11-08 21:10:32 +0800

    Adjust the layout

--- src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
+++ src/app/(main)/(billing)/billing/create/BillingCreateContent.tsx
@@ -4,7 +4,15 @@ import BillingCatalogSearch from '@/components/billing/catalog-search/BillingCat
 import { SearchState } from '@/components/billing/catalog-search/BillingCatalogSearch'
 import InputBilling from '@/components/billing/input/InputBilling'
 import { Billing } from '@/types/Billing'
-import { Box, Button, Flex, useDisclosure } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  Divider,
+  Flex,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { useState } from 'react'
 import BillingCreateSearchDialog from './BillingCreateSearchDialog'
 
@@ -23,25 +31,30 @@ export default function BillingCreateContent({
 
   return (
     <>
-      <Flex width="full" height="full" gap={2}>
-        <Flex flex={1} direction="column">
-          <Flex flex={1} position="relative">
-            <Box
-              position="absolute"
-              width="full"
-              height="full"
-              overflowY="auto"
-            >
-              <InputBilling billing={billing} onChange={setBilling} />
-            </Box>
-          </Flex>
+      <Flex width="full" height="full" gap={2} direction="column">
+        <Flex flex={1} gap={2}>
+          <Card flex={1}>
+            <CardBody as={Flex} direction="column" gap={2}>
+              <Flex flex={1}>
+                <InputBilling billing={billing} onChange={setBilling} />
+              </Flex>
 
-          <form action={() => onSave(billing)}>
-            <Button width="full" colorScheme="blue" type="submit">
-              Save
-            </Button>
-          </form>
+              <Divider />
+
+              <Button onClick={onOpen}>Add Items</Button>
+            </CardBody>
+          </Card>
+
+          <Card flex={1}>
+            <CardBody></CardBody>
+          </Card>
         </Flex>
+
+        <form action={() => onSave(billing)}>
+          <Button width="full" colorScheme="blue" type="submit">
+            Save
+          </Button>
+        </form>
       </Flex>
 
       <BillingCreateSearchDialog isOpen={isOpen} onClose={onClose}>
