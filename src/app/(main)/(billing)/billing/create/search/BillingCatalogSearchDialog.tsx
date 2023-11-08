b2c36d7456af10bfb65e625eb7ac4a644937089c
Felix <jonfelixrico@gmail.com>
2023-11-08 21:44:31 +0800

    Expand the functionality

--- src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialog.tsx
+++ src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialog.tsx
@@ -1,6 +1,9 @@
+'use client '
+
 import {
   Button,
   Flex,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,10 +12,28 @@ import {
   ModalHeader,
   ModalOverlay,
   ModalProps,
+  Text,
 } from '@chakra-ui/react'
 import { SearchState, useSearch } from './useSearch'
 import BillingCatalogSearchItem from './BillingCatalogSearchItem'
 import { CatalogItem } from '@/types/CatalogItem'
+import { useState } from 'react'
+import If from '@/components/common/If'
+
+function SearchBar({
+  triggerSearch,
+}: {
+  triggerSearch: (searchTerm: string) => void
+}) {
+  const [input, setInput] = useState('')
+
+  return (
+    <>
+      <Input value={input} onChange={(event) => setInput(event.target.value)} />
+      <Button onClick={() => triggerSearch(input)}>Search</Button>
+    </>
+  )
+}
 
 type BillingCatalogSearchDialogProps = {
   initialState: SearchState
@@ -29,10 +50,13 @@ export default function BillingCreateSearchDialog({
 }: BillingCatalogSearchDialogProps) {
   const { items, canLoadMore, startSearch, loadMore } = useSearch(initialState)
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Add Item</ModalHeader>
+        <ModalHeader>
+          <Text>Add Item</Text>
+          <SearchBar triggerSearch={startSearch} />
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Flex direction="column" gap={3}>
@@ -44,6 +68,10 @@ export default function BillingCreateSearchDialog({
                 canAdd={!cannotAdd.has(item.id)}
               />
             ))}
+
+            <If condition={canLoadMore}>
+              <Button onClick={loadMore}>Load More</Button>
+            </If>
           </Flex>
         </ModalBody>
 
