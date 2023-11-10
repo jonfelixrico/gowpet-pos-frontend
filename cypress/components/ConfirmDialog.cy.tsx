258dcc5c203de434c343e6a8e5aa0f918a45f291
Felix <jonfelixrico@gmail.com>
2023-11-10 22:26:34 +0800

    ci: billing: add tests (#33)
    * Add tests for if
    
    * Create tests for ConfirmDialog
    
    * Adjust tests
    
    * Satisfy the tests
    
    * Add test
    
    * Add test

---
+++ cypress/components/ConfirmDialog.cy.tsx
@@ -0,0 +1,84 @@
+import ConfirmDialog from '@/components/common/ConfirmDialog'
+import { ChakraProvider } from '@chakra-ui/react'
+
+describe('ConfirmDialog', () => {
+  it('shows defaults', () => {
+    cy.mount(
+      <ChakraProvider>
+        <ConfirmDialog
+          isOpen={true}
+          onOk={() => {}}
+          onCancel={() => {}}
+          onDismiss={() => {}}
+          title="title"
+          message="message"
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-cy="ok"]').should('contain.text', 'Ok')
+    cy.get('[data-cy="cancel"]').should('contain.text', 'Cancel')
+    cy.get('[data-cy="title"]').should('contain.text', 'title')
+    cy.get('[data-cy="message"]').should('contain.text', 'message')
+  })
+
+  it('shows custom button labels', () => {
+    cy.mount(
+      <ChakraProvider>
+        <ConfirmDialog
+          isOpen={true}
+          onOk={() => {}}
+          onCancel={() => {}}
+          onDismiss={() => {}}
+          title="title"
+          message="message"
+          ok={{
+            label: 'custom ok',
+          }}
+          cancel={{
+            label: 'custom cancel',
+          }}
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-cy="ok"]').should('contain.text', 'custom ok')
+    cy.get('[data-cy="cancel"]').should('contain.text', 'custom cancel')
+  })
+
+  it('shows custom button labels', () => {
+    const onOk = cy.spy()
+    const onCancel = cy.spy()
+
+    cy.mount(
+      <ChakraProvider>
+        <ConfirmDialog
+          isOpen={true}
+          onOk={onOk}
+          onCancel={onCancel}
+          onDismiss={() => {}}
+          title="title"
+          message="message"
+          ok={{
+            label: 'custom ok',
+          }}
+          cancel={{
+            label: 'custom cancel',
+          }}
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-cy="ok"]')
+      .click()
+      .should(() => {
+        expect(onOk).to.have.been.calledOnce
+      })
+
+    cy.get('[data-cy="cancel"]')
+      .click()
+      .should(() => {
+        expect(onCancel).to.have.been.calledOnce
+      })
+  })
+})
