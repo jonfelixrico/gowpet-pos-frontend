2bdc505f03cce8df478da233f4c5c9971e7fb9c8
Felix <jonfelixrico@gmail.com>
2023-11-11 00:32:59 +0800

    Adjust UI

--- src/app/(main)/(billing)/billing/create/input/BillingItemEditDialog.tsx
+++ src/app/(main)/(billing)/billing/create/input/BillingItemEditDialog.tsx
@@ -1,11 +1,13 @@
 import Dialog from '@/components/common/Dialog'
 import { BillingItem } from '@/types/Billing'
 import {
+  Flex,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
+  Text,
 } from '@chakra-ui/react'
 import { useState } from 'react'
 
@@ -38,18 +40,30 @@ export default function BillingItemEditDialog({
       onOk={emitValue}
       onCancel={onDismiss}
       onDismiss={onDismiss}
+      ok={{
+        label: 'Save Changes',
+        colorScheme: 'blue',
+      }}
+      cancel={{
+        label: 'Cancel Changes',
+        variant: 'ghost',
+      }}
+      header="Edit Item"
     >
-      <NumberInput
-        value={quantity}
-        onChange={(inputValue) => setQuantity(parseInt(inputValue))}
-        min={1}
-      >
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+      <Flex direction="column" gap="2">
+        <Text fontWeight="bold">Quantity</Text>
+        <NumberInput
+          value={quantity}
+          onChange={(inputValue) => setQuantity(parseInt(inputValue))}
+          min={1}
+        >
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </Flex>
     </Dialog>
   )
 }
