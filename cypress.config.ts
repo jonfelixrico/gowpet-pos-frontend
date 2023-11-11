d43047af2b3f73a1092619336b5a43bd7948c7f3
Felix <jonfelixrico@gmail.com>
2023-11-11 15:44:09 +0800

    Relocate opt in the e2e test only

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+
+    /*
+     * We've encountered quite a bit of times where E2E tests ran in GitHub action runners fail because of timeouts.
+     * We can just restart those tests, but rerunning even just the failing tests can be time-consuming especially as the project
+     * scales up.
+     *
+     * Increasing the timeout is sort of like a band-aid fix where we don't really have a choice.
+     */
+    defaultCommandTimeout: 30_000,
   },
 
   component: {
@@ -11,12 +20,4 @@ export default defineConfig({
       bundler: 'webpack',
     },
   },
-
-  /*
-   * We've encountered quite a bit of times where E2E tests ran in GitHub action runners fail because of timeouts.
-   * It could be that the runner is slow since it's something like a shared instance.
-   *
-   * By increasing the command timeout, we're hoping that we will encounter less E2E failures due to slow runners.
-   */
-  defaultCommandTimeout: 30_000,
 })
