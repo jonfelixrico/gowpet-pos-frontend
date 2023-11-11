b6589ae354d1d951f874ef695f7136bb06865bd5
Felix <jonfelixrico@gmail.com>
2023-11-11 17:06:40 +0800

    Satisfy the tests

--- src/app/(main)/(billing)/billing/[id]/not-found.tsx
+++ src/app/(main)/(billing)/billing/[id]/not-found.tsx
@@ -3,5 +3,5 @@ import { Box } from '@chakra-ui/react'
 export default function Billing404() {
   // TODO impl the actual logic here
 
-  return <Box>Not Found</Box>
+  return <Box data-cy="not-found">Not Found</Box>
 }
