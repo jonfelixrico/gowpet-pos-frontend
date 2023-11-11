36c184d78ab5d0095390eefe8fa19554ee6de7c5
Felix <jonfelixrico@gmail.com>
2023-11-11 15:41:56 +0800

    Add comment behind intention

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
     },
   },
 
+  /*
+   * We've encountered quite a bit of times where E2E tests ran in GitHub action runners fail because of timeouts.
+   * It could be that the runner is slow since it's something like a shared instance.
+   *
+   * By increasing the command timeout, we're hoping that we will encounter less E2E failures due to slow runners.
+   */
   defaultCommandTimeout: 30_000,
 })
