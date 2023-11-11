a642a280c3a4b8edcd76c69ed914258d8659b41f
Felix <jonfelixrico@gmail.com>
2023-11-11 16:02:38 +0800

    Create billing details page

---
+++ src/app/(main)/(billing)/billing/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import { Billing } from '@/types/Billing'
+import { FetchError } from '@/utils/fetch-utils'
+import { Box } from '@chakra-ui/react'
+import { notFound } from 'next/navigation'
+
+export default async function Billing({
+  params,
+}: {
+  params: {
+    id: string
+  }
+}) {
+  let data: Billing
+  try {
+    const response = await apiFetchData(`/billing/${params.id}`)
+    data = response.data
+  } catch (e) {
+    if (e instanceof FetchError && e.response.status === 404) {
+      notFound()
+    }
+
+    throw e
+  }
+
+  return <Box>{JSON.stringify(data)}</Box>
+}
