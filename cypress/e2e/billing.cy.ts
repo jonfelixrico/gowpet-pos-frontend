f2f0a6c6b8eff8eb4f7c3e01a8624cb385159d27
Felix <jonfelixrico@gmail.com>
2023-11-11 22:04:28 +0800

    Populate catalog items

--- cypress/e2e/billing.cy.ts
+++ cypress/e2e/billing.cy.ts
@@ -1,4 +1,28 @@
 describe('billing', () => {
+  const now = Date.now()
+
+  before(() => {
+    const items: Record<number, string> = {}
+    for (let i = 0; i < 20; i++) {
+      cy.request({
+        url: 'http://localhost:3005/catalog/product',
+        method: 'POST',
+
+        body: JSON.stringify({
+          name: `for billing e2e - ${now} - ${i}`,
+          price: 150 + i * 10,
+        }),
+
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${Cypress.env('authToken')}`,
+        },
+      }).then((res) => {
+        items[i] = res.body.id
+      })
+    }
+  })
+
   it('shows 404 page for non-existent billings', () => {
     cy.visit('/billing/this-does-not-exist')
 
