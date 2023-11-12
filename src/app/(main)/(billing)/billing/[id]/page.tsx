831c32324445214b87872e4b69c6086920865fc2
Felix <jonfelixrico@gmail.com>
2023-11-12 19:34:18 +0800

    Use component in the page

--- src/app/(main)/(billing)/billing/[id]/page.tsx
+++ src/app/(main)/(billing)/billing/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { apiFetchData } from '@/server-utils/resource-api-util'
 import { Billing } from '@/types/Billing'
 import { FetchError } from '@/utils/fetch-utils'
-import { Box } from '@chakra-ui/react'
+import { Box, Card, CardBody, Flex, IconButton, Text } from '@chakra-ui/react'
 import { notFound } from 'next/navigation'
+import BillingDetailsItemSection from './BillingDetailsItemSection'
+import Link from 'next/link'
+import { IoIosArrowBack } from 'react-icons/io'
 
 export default async function Billing({
   params,
@@ -23,6 +26,26 @@ export default async function Billing({
     throw e
   }
 
-  // TODO implement the actual content
-  return <Box data-cy="content">{JSON.stringify(data)}</Box>
+  const { items } = data
+
+  return (
+    <Flex direction="column" gap="2">
+      <Card>
+        <CardBody as={Flex} gap={2}>
+          <Flex gap={2}>
+            <Link href="/billing">
+              <IconButton aria-label="go back to billings list">
+                <IoIosArrowBack />
+              </IconButton>
+            </Link>
+
+            <Text>Billing Information</Text>
+          </Flex>
+        </CardBody>
+
+        {/* TODO add notes */}
+      </Card>
+      <BillingDetailsItemSection items={items} />
+    </Flex>
+  )
 }
