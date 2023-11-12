bfe95d71f5de02f3512c72b8dcc94008304730cf
Felix <jonfelixrico@gmail.com>
2023-11-13 01:30:00 +0800

    Use paginated

--- src/app/(main)/(billing)/(billing-list)/billing/page.tsx
+++ src/app/(main)/(billing)/(billing-list)/billing/page.tsx
@@ -1,13 +1,32 @@
 import { apiFetchData } from '@/server-utils/resource-api-util'
 import { Billing } from '@/types/Billing'
 import { Box, Text } from '@chakra-ui/react'
+import { stringify } from 'querystring'
 
-export default async function BillingListPage() {
-  const { data: billings } = await apiFetchData<Billing[]>('/billing')
+export default async function BillingListPage({
+  searchParams,
+}: {
+  searchParams: {
+    pageNo?: string
+  }
+}) {
+  const queryParams = stringify({
+    pageNo: searchParams.pageNo ?? '0',
+  })
+  const { data, headers } = await apiFetchData<Billing[]>(
+    `/billing?${queryParams}`
+  )
+
+  const xTotalCount = headers.get('X-Total-Count')
+  if (xTotalCount === null) {
+    throw new Error('X-Total-Count was not found in the backend response')
+  }
+  const pageCount = parseInt(xTotalCount)
+
   return (
     <Box>
       {/* TODO implement listing */}
-      {billings.map((billing, index) => (
+      {data.map((billing, index) => (
         <Text key={index}>{JSON.stringify(billing)}</Text>
       ))}
     </Box>
