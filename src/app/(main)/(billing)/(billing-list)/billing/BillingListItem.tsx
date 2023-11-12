8206e2837f3584ac28264a73fa8eafcf42def35c
Felix <jonfelixrico@gmail.com>
2023-11-13 01:56:56 +0800

    Adjust layout

--- src/app/(main)/(billing)/(billing-list)/billing/BillingListItem.tsx
+++ src/app/(main)/(billing)/(billing-list)/billing/BillingListItem.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Card,
   CardBody,
+  Divider,
   Flex,
   Spacer,
   Text,
@@ -16,18 +17,41 @@ export default function BillingListItem({
 }: {
   billing: SavedBilling
 }) {
+  const totalAmount = billing.items.reduce(
+    (total, billing) => total + billing.price * billing.quantity,
+    0
+  )
+
+  const totalQuantity = billing.items.reduce(
+    (total, billing) => total + billing.quantity,
+    0
+  )
+
   return (
     <Card>
-      <CardBody as={Flex} gap={2} align="center">
-        <Box flex={1}>
-          <If condition={!billing.notes}>
-            <Text noOfLines={2}>{billing.notes}</Text>
-          </If>
-        </Box>
+      <CardBody as={Flex} gap={2} direction="column">
+        <Flex gap={2} align="center">
+          <Flex flex={1} direction="column" gap={2}>
+            <Text>{totalAmount}</Text>
 
-        <Link href={`/billing/${billing.id}`} prefetch={false}>
-          <Button variant="ghost">Open</Button>
-        </Link>
+            <Text>
+              {totalQuantity} {totalQuantity === 1 ? 'item' : 'items'}
+            </Text>
+          </Flex>
+
+          <Link href={`/billing/${billing.id}`} prefetch={false}>
+            <Button variant="ghost">Open</Button>
+          </Link>
+        </Flex>
+
+        <If condition={!!billing.notes}>
+          <Box background="gray.100" padding={3} borderRadius="md">
+            <Text fontWeight="bold" fontSize="xs">
+              Notes
+            </Text>
+            <Text noOfLines={2}>{billing.notes}</Text>
+          </Box>
+        </If>
       </CardBody>
     </Card>
   )
