ddcc3c159e8afc71ef0fd8c861503058fbc17c7c
Felix <jonfelixrico@gmail.com>
2023-11-12 20:31:08 +0800

    Adjust the UI for the not found page

--- src/app/(main)/(billing)/billing/[id]/not-found.tsx
+++ src/app/(main)/(billing)/billing/[id]/not-found.tsx
@@ -1,11 +1,27 @@
-import { Center, Text } from '@chakra-ui/react'
+import { Button, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Billing404() {
   // TODO impl the actual logic here
 
   return (
-    <Center height="full" width="full" data-cy="not-found">
-      <Text>Not Found</Text>
-    </Center>
+    <Flex
+      direction="column"
+      justify="center"
+      align="center"
+      height="full"
+      width="full"
+      data-cy="not-found"
+      gap={3}
+    >
+      <Text fontSize="2xl">
+        The billing that you tried to access does not exist
+      </Text>
+      <Link href="/billing">
+        <Button colorScheme="blue" variant="ghost">
+          Go back to the billing list
+        </Button>
+      </Link>
+    </Flex>
   )
 }
