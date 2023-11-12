2ba6be327bc9b2af33b506d2f4324894642f3336
Felix <jonfelixrico@gmail.com>
2023-11-12 20:09:38 +0800

    Fix tests

--- cypress/components/BillingDetailsItemTableRow.cy.tsx
+++ cypress/components/BillingDetailsItemTableRow.cy.tsx
@@ -1,18 +1,23 @@
 import { BillingDetailsItemTableRow } from '@/app/(main)/(billing)/billing/[id]/BillingDetailsItemTable'
+import { ChakraProvider, Table } from '@chakra-ui/react'
 
 describe('BillingDetailsItemTableRow', () => {
   it('it should show the correct data', () => {
     cy.mount(
-      <BillingDetailsItemTableRow
-        item={{
-          catalogItem: {
-            id: 'dummy',
-            name: 'test name',
-          },
-          price: 150.5,
-          quantity: 3,
-        }}
-      />
+      <ChakraProvider>
+        <Table>
+          <BillingDetailsItemTableRow
+            item={{
+              catalogItem: {
+                id: 'dummy',
+                name: 'test name',
+              },
+              price: 150.5,
+              quantity: 3,
+            }}
+          />
+        </Table>
+      </ChakraProvider>
     )
 
     cy.get('[data-cy="name"]').should('have.text', 'test name')
