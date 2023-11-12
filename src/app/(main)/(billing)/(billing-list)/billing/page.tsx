88d33ad61a0545b8f7a29fbb6aea44f2275a224d
Felix <jonfelixrico@gmail.com>
2023-11-13 02:14:45 +0800

    Create layout for the list

--- src/app/(main)/(billing)/(billing-list)/billing/page.tsx
+++ src/app/(main)/(billing)/(billing-list)/billing/page.tsx
@@ -1,5 +1,5 @@
 import { apiFetchData } from '@/server-utils/resource-api-util'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Flex, FlexProps } from '@chakra-ui/react'
 import { stringify } from 'querystring'
 import BillingListItem from './BillingListItem'
 import { SavedBilling } from '../../BillingDetailsData'
@@ -7,6 +7,47 @@ import {
   PaginationControls,
   Url,
 } from '@/components/pagination/PaginationControls'
+import { ReactNode } from 'react'
+
+function PaginationLayout({
+  pageCount,
+  pageNo,
+  children,
+  ...flexProps
+}: {
+  pageCount: number
+  pageNo: number
+  children: ReactNode
+} & FlexProps) {
+  function hrefBuilder(pageNo: number): Url {
+    return {
+      pathname: '/billing',
+      query: {
+        pageNo,
+      },
+    }
+  }
+
+  return (
+    <Flex {...flexProps} direction="column" gap={2}>
+      <PaginationControls
+        hrefBuilder={hrefBuilder}
+        pageCount={pageCount}
+        pageNo={pageNo}
+      />
+
+      <Flex direction="column" flex={1} gap={2}>
+        {children}
+      </Flex>
+
+      <PaginationControls
+        hrefBuilder={hrefBuilder}
+        pageCount={pageCount}
+        pageNo={pageNo}
+      />
+    </Flex>
+  )
+}
 
 export default async function BillingListPage({
   searchParams: { pageNo = '1' },
@@ -34,33 +75,15 @@ export default async function BillingListPage({
     throw new Error('X-Total-Count was not found in the backend response')
   }
 
-  function hrefBuilder(pageNo: number): Url {
-    return {
-      pathname: '/billing',
-      query: {
-        pageNo,
-      },
-    }
-  }
-
   return (
-    <Flex direction="column" gap={2} width="full">
-      <PaginationControls
-        hrefBuilder={hrefBuilder}
-        pageCount={parseInt(xTotalCount)}
-        pageNo={parsedPageNo}
-      />
-
-      {/* TODO implement listing */}
+    <PaginationLayout
+      pageCount={parseInt(xTotalCount)}
+      pageNo={parsedPageNo}
+      width="full"
+    >
       {data.map((billing) => (
         <BillingListItem billing={billing} key={billing.id} />
       ))}
-
-      <PaginationControls
-        hrefBuilder={hrefBuilder}
-        pageCount={parseInt(xTotalCount)}
-        pageNo={parsedPageNo}
-      />
-    </Flex>
+    </PaginationLayout>
   )
 }
