c886ccbb5cbf291ccb82a078c20fcde3f8148465
Felix <jonfelixrico@gmail.com>
2023-11-12 19:29:30 +0800

    Create the item section

---
+++ src/app/(main)/(billing)/billing/[id]/BillingDetailsItemSection.tsx
@@ -0,0 +1,39 @@
+import If from '@/components/common/If'
+import { BillingItem } from '@/types/Billing'
+import { Box, Card, CardBody, Flex, Text } from '@chakra-ui/react'
+import BillingDetailsItemTable from './BillingDetailsItemTable'
+
+export default function BillingDetailsItemSection({
+  items,
+}: {
+  items: BillingItem[]
+}) {
+  const hasItems = !!items.length
+  const total = items.reduce(
+    (total, { price, quantity }) => total + price * quantity,
+    0
+  )
+
+  return (
+    <Card>
+      <CardBody>
+        <If condition={hasItems}>
+          <BillingDetailsItemTable items={items} />
+        </If>
+
+        <If condition={!hasItems}>
+          <Flex justify="center" align="center" height="50dvh">
+            <Text>There are no items</Text>
+          </Flex>
+        </If>
+
+        <Box>
+          <Flex justify="space-between">
+            <Text>Total</Text>
+            <Text>{total}</Text>
+          </Flex>
+        </Box>
+      </CardBody>
+    </Card>
+  )
+}
