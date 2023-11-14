716704ba50bd4c2ccf274cf98fab633864774e21
Felix <jonfelixrico@gmail.com>
2023-11-15 01:40:13 +0800

    Add test

--- cypress/components/BillingItemTable.cy.tsx
+++ cypress/components/BillingItemTable.cy.tsx
@@ -8,7 +8,7 @@ function generateDummyItems(count: number, startId = 0): BillingItem[] {
     .map((_, index) => {
       const id = startId + index
       return {
-        catalogId: `item ${id}`,
+        catalogId: `item-${id}`,
         name: `item ${id} name`,
         price: 150.0,
         quantity: 25,
@@ -39,5 +39,41 @@ describe('BillingItemTable', () => {
       'data-dialog-opened',
       'true'
     )
+  })
+
+  it('handles delete', () => {
+    const setBilling = cy.spy().as('setBilling')
+
+    const items = generateDummyItems(10)
+    cy.mount(
+      <ChakraProvider>
+        <BillingItemTable
+          billing={{
+            notes: '',
+            items,
+          }}
+          setBilling={setBilling}
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-billing-item-id="item-5"] [data-cy="delete"]').click()
+
+    cy.get(
+      '[data-cy="delete-confirm"][data-dialog-opened="true"] [data-cy="ok"]'
+    ).click()
+    cy.get('[data-cy="delete-confirm"]').should(
+      'have.attr',
+      'data-dialog-opened',
+      'true'
+    )
+
+    const clone = [...items]
+    clone.splice(5, 1)
+
+    cy.get('@setBilling').should('have.been.calledOnceWith', {
+      notes: '',
+      items: clone,
+    })
   })
 })
