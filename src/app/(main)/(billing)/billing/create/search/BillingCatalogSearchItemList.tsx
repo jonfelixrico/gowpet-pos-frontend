cad4af1e8b668b6d18849b361cb81db678c8029e
Felix <jonfelixrico@gmail.com>
2023-11-14 21:46:51 +0800

    Create simplified component for the list

---
+++ src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchItemList.tsx
@@ -0,0 +1,73 @@
+import { Billing } from '@/types/Billing'
+import { CatalogItem } from '@/types/CatalogItem'
+import { DataAttributes } from '@/types/DataAttributes'
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { produce } from 'immer'
+import { Dispatch, SetStateAction } from 'react'
+
+function BillingCatalogSearchItem({
+  canAdd,
+  onAdd,
+  name,
+  price,
+  ...dataAttrs
+}: {
+  name: string
+  price: number
+  onAdd: () => void
+  canAdd: boolean
+} & DataAttributes) {
+  return (
+    <Flex {...dataAttrs} justify="space-between" gap={2}>
+      <Box>
+        <Text>{name}</Text>
+        <Text fontSize="xs">{price}</Text>
+      </Box>
+
+      <Button isDisabled={!canAdd} onClick={onAdd} data-cy="add">
+        Add
+      </Button>
+    </Flex>
+  )
+}
+
+export default function BillingCatalogSearchItemList({
+  billing,
+  setBilling,
+  itemsToSelectFrom,
+}: {
+  billing: Billing
+  setBilling: Dispatch<SetStateAction<Billing>>
+  itemsToSelectFrom: CatalogItem[]
+}) {
+  const alreadyAddedIds = new Set<string>(
+    billing.items.map(({ catalogId }) => catalogId)
+  )
+
+  function addCatalogItemToBilling({ id, name, price }: CatalogItem) {
+    setBilling((billing) =>
+      produce(billing, (billing) => {
+        billing.items.push({
+          catalogId: id,
+          name,
+          price,
+          quantity: 1,
+        })
+      })
+    )
+  }
+
+  return itemsToSelectFrom.map((catalogItem) => {
+    const { id, name, price } = catalogItem
+
+    return (
+      <BillingCatalogSearchItem
+        name={name}
+        price={price}
+        onAdd={() => addCatalogItemToBilling(catalogItem)}
+        key={id}
+        canAdd={!alreadyAddedIds.has(id)}
+      />
+    )
+  })
+}
