3b10ac7861a2c2c2a65242dd8c90b0ea81678bbc
Felix <jonfelixrico@gmail.com>
2023-11-15 00:13:06 +0800

    Add more tests

--- cypress/components/BillingCatalogSearchDialogContent.cy.tsx
+++ cypress/components/BillingCatalogSearchDialogContent.cy.tsx
@@ -1,4 +1,5 @@
 import BillingCatalogSearchDialogContent from '@/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialogContent'
+import { CatalogItem } from '@/types/CatalogItem'
 import { ChakraProvider, Modal } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
@@ -12,6 +13,20 @@ function Wrapper({ children }: { children: ReactNode }) {
   )
 }
 
+function generateDummyItems(count: number, startId = 0): CatalogItem[] {
+  return Array.from({ length: count })
+    .fill(null)
+    .map((_, index) => {
+      const id = startId + index
+      return {
+        id: `item ${id}`,
+        name: `item ${id} name`,
+        price: 150.0,
+        type: 'PRODUCT',
+      }
+    })
+}
+
 describe('BillingCatalogSearchDialogContent', () => {
   it('shows empty message', () => {
     cy.mount(
@@ -20,5 +35,53 @@ describe('BillingCatalogSearchDialogContent', () => {
       </Wrapper>
     )
     cy.get('[data-cy="no-items"]').should('exist')
+  })
+
+  it('does not render the show more button', () => {
+    cy.mount(
+      <Wrapper>
+        <BillingCatalogSearchDialogContent
+          initialState={{
+            items: generateDummyItems(1),
+            pageCount: 10,
+            pageNo: 9,
+            searchTerm: '',
+          }}
+        />
+      </Wrapper>
+    )
+
+    cy.get('[data-cy="show-more"]').should('not.exist')
+  })
+
+  it('can load more', () => {
+    cy.mount(
+      <Wrapper>
+        <BillingCatalogSearchDialogContent
+          initialState={{
+            items: generateDummyItems(10),
+            pageCount: 10,
+            pageNo: 8,
+            searchTerm: '',
+          }}
+        />
+      </Wrapper>
+    )
+
+    cy.get('[data-cy="catalog-item"]').should('have.length', 10)
+
+    cy.intercept('/billing/catalog-search*', {
+      body: generateDummyItems(10, 10),
+      headers: {
+        'X-Total-Count': '10',
+      },
+    })
+
+    cy.get('[data-cy="show-more"]').click()
+
+    cy.get('[data-cy="catalog-item"]').should('have.length', 20)
+
+    // should be at the final page at this point
+    cy.get('[data-cy="show-more"]').should('not.exist')
   })
 })
