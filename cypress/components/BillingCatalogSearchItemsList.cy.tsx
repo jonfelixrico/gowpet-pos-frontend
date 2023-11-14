48ed76474b64fb3ee884e28d5c61b58f7beb9253
Felix <jonfelixrico@gmail.com>
2023-11-14 22:21:47 +0800

    Add more tests

--- cypress/components/BillingCatalogSearchItemsList.cy.tsx
+++ cypress/components/BillingCatalogSearchItemsList.cy.tsx
@@ -21,4 +21,105 @@ describe('BillingCatalogSearchItemsList', () => {
     cy.get('[data-cy="name"]').should('have.text', 'test name')
     cy.get('[data-cy="price"]').should('have.text', '123.45')
   })
+
+  it('adds to the billing items', () => {
+    const setBilling = cy.spy().as('setBilling')
+
+    cy.mount(
+      <ChakraProvider>
+        <BillingCatalogSearchItemsList
+          itemsToSelectFrom={[
+            {
+              id: 'test 1',
+              name: 'test name 1',
+              price: 123.45,
+              type: 'PRODUCT',
+            },
+            {
+              id: 'test 2',
+              name: 'test name 2',
+              price: 234.56,
+              type: 'PRODUCT',
+            },
+          ]}
+          billing={{
+            items: [
+              {
+                catalogId: 'test 1',
+                name: 'test name 1',
+                price: 123.45,
+                quantity: 1,
+              },
+            ],
+
+            notes: '',
+          }}
+          setBilling={setBilling}
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-catalog-item-id="test 2"] [data-cy="add"]').click()
+
+    cy.get('@setBilling').should('have.been.calledOnceWith', {
+      items: [
+        {
+          catalogId: 'test 1',
+          name: 'test name 1',
+          price: 123.45,
+          quantity: 1,
+        },
+        {
+          catalogId: 'test 2',
+          name: 'test name 2',
+          price: 234.56,
+          quantity: 1,
+        },
+      ],
+
+      notes: '',
+    })
+  })
+
+  it('prevents adding already-added items', () => {
+    cy.mount(
+      <ChakraProvider>
+        <BillingCatalogSearchItemsList
+          itemsToSelectFrom={[
+            {
+              id: 'test 1',
+              name: 'test name 1',
+              price: 123.45,
+              type: 'PRODUCT',
+            },
+            {
+              id: 'test 2',
+              name: 'test name 2',
+              price: 234.56,
+              type: 'PRODUCT',
+            },
+          ]}
+          billing={{
+            items: [
+              {
+                catalogId: 'test 1',
+                name: 'test name 1',
+                price: 123.45,
+                quantity: 1,
+              },
+            ],
+
+            notes: '',
+          }}
+        />
+      </ChakraProvider>
+    )
+
+    cy.get('[data-catalog-item-id="test 1"] [data-cy="add"]').should(
+      'be.disabled'
+    )
+    cy.get('[data-catalog-item-id="test 2"] [data-cy="add"]').should(
+      'not.be.disabled'
+    )
+  })
 })
