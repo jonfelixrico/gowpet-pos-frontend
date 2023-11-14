30f2305484d79859e86c7f662b21dcb1ed2f18c0
Felix <jonfelixrico@gmail.com>
2023-11-14 23:29:27 +0800

    Create content

---
+++ src/app/(main)/(billing)/billing/create/search/BillingCatalogSearchDialogContent.tsx
@@ -0,0 +1,89 @@
+'use client '
+
+import {
+  Button,
+  Center,
+  Flex,
+  Input,
+  ModalBody,
+  ModalCloseButton,
+  ModalHeader,
+  Text,
+} from '@chakra-ui/react'
+import { SearchState, useSearch } from './useSearch'
+import { Dispatch, SetStateAction, useState } from 'react'
+import If from '@/components/common/If'
+import { Billing } from '@/types/Billing'
+import BillingCatalogSearchItemsList from './BillingCatalogSearchItemsList'
+
+function SearchBarContent({
+  triggerSearch,
+  isLoading,
+}: {
+  triggerSearch: (searchTerm: string) => void
+  isLoading: boolean
+}) {
+  const [input, setInput] = useState('')
+
+  return (
+    <>
+      <Input
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        isReadOnly={isLoading}
+      />
+      <Button isLoading={isLoading} onClick={() => triggerSearch(input)}>
+        Search
+      </Button>
+    </>
+  )
+}
+
+export type BillingCatalogSearchDialogProps = {
+  initialState: SearchState
+  billing: Billing
+  setBilling: Dispatch<SetStateAction<Billing>>
+}
+
+export default function BillingCreateSearchDialog({
+  initialState,
+  billing,
+  setBilling,
+}: BillingCatalogSearchDialogProps) {
+  const { items, canLoadMore, startSearch, loadMore, isLoading } =
+    useSearch(initialState)
+
+  return (
+    <>
+      <ModalHeader as={Flex} direction="column" gap={5}>
+        <Text>Add Item</Text>
+        <Flex gap={2} align="center" data-cy="search">
+          <SearchBarContent triggerSearch={startSearch} isLoading={isLoading} />
+        </Flex>
+      </ModalHeader>
+      <ModalCloseButton />
+
+      <ModalBody>
+        <Flex direction="column" gap={3}>
+          <If condition={items.length > 0}>
+            <BillingCatalogSearchItemsList
+              billing={billing}
+              setBilling={setBilling}
+              itemsToSelectFrom={items}
+            />
+
+            <If condition={canLoadMore}>
+              <Button onClick={loadMore} isLoading={isLoading}>
+                Load More
+              </Button>
+            </If>
+          </If>
+
+          <If condition={items.length === 0}>
+            <Center width="full">No items to show</Center>
+          </If>
+        </Flex>
+      </ModalBody>
+    </>
+  )
+}
