51928128e3f2b0a496cd00eb541ec10b4fdbedc5
Felix <jonfelixrico@gmail.com>
2023-11-15 22:13:40 +0800

    Show serial no

--- src/app/(main)/(billing)/(billing-list)/billing/BillingListItem.tsx
+++ src/app/(main)/(billing)/(billing-list)/billing/BillingListItem.tsx
@@ -24,13 +24,18 @@ export default function BillingListItem({
     <Card {...dataProps}>
       <CardBody as={Flex} gap={2} direction="column">
         <Flex gap={2} align="center">
-          <Flex flex={1} direction="column" gap={2}>
-            <Text data-cy="total-amount">{totalAmount}</Text>
-
-            <Text data-cy="total-quantity">
-              {totalQuantity} {totalQuantity === 1 ? 'item' : 'items'}
+          <Box flex={1}>
+            <Text fontSize="xl" fontWeight="bold">
+              No. <span>{String(billing.serialNo).padStart(4, '0')}</span>
             </Text>
-          </Flex>
+            <Text fontSize="sm">
+              <span data-cy="total-amount">{totalAmount}</span>,{' '}
+              <span>
+                <span data-cy="total-quantity">{totalQuantity}</span>{' '}
+                {totalQuantity === 1 ? 'item' : 'items'}
+              </span>
+            </Text>
+          </Box>
 
           {/* Prefetch needs to be false or else we'll end up preloading all of the
           items in the list, which is going to probably request a lot of stuff from the
