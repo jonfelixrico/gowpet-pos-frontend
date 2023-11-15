eac84946c859f63fbba241bde55e31253e7778bd
Felix <jonfelixrico@gmail.com>
2023-11-15 23:45:25 +0800

    ci: billing: complete billing create flow in the e2e tests (#52)
    * Change condition
    
    * Add notes to test
    
    * Satisfy notes
    
    * Add tests for confirming contents of the created billing
    
    * Make the quantity have more variety
    
    * Adjust tests

--- cypress/e2e/billing.cy.ts
+++ cypress/e2e/billing.cy.ts
@@ -52,7 +52,7 @@ describe('billing', () => {
     )
     cy.get('[data-cy="add-items-dialog"] [data-cy="search"] button').click()
 
-    const toAdd = items.filter((_, index) => index % 2 === 0)
+    const toAdd = items.slice(0, 5)
 
     for (const { id } of toAdd) {
       cy.get(
@@ -64,34 +64,45 @@ describe('billing', () => {
     // check if the items in the billing items table checks out with what we added
 
     cy.get('[data-cy="billing-item"]').should('have.length', toAdd.length)
-    for (const { id, price } of toAdd) {
-      // this makes sure that we're showing the critical information per item
 
-      cy.get(`[data-billing-item-id="${id}"] [data-cy="price"]`).should(
-        'contain',
-        price
-      )
+    toAdd.forEach(({ id }, index) => {
+      for (let i = 0; i < index; i++) {
+        cy.get(`[data-billing-item-id="${id}"] [data-cy="increment"]`).click()
+      }
+
       cy.get(`[data-billing-item-id="${id}"] [data-cy="quantity"]`).should(
         'contain',
-        1
+        1 + index
       )
-      cy.get(`[data-billing-item-id="${id}"] [data-cy="amount"]`).should(
-        'contain',
-        price
-      )
-    }
+    })
 
-    // this makes sure that we're displaying the correct billing-wide information
-    cy.get('[data-cy="total-amount"]').should(
-      'contain',
-      toAdd.reduce((total, { price }) => total + price, 0)
-    )
+    cy.get('[data-cy="notes"]').type('test notes')
 
     // test that saving works
     cy.get('[data-cy="save"]').click()
     cy.get(
       '[data-cy="save-confirmation-dialog"][data-dialog-open="true"] [data-cy="ok"]'
     ).click()
-    cy.location('pathname').should('match', /\/billing\/.+$/)
+
+    cy.location('pathname').should('not.equal', '/billing/create')
+    cy.location('pathname').should('match', /\/billing\/\S+$/)
+
+    cy.get('[data-cy="notes"]').should('have.value', 'test notes')
+    cy.get('[data-cy="items-table"] [data-cy="row"]').should('have.length', 5)
+
+    toAdd.forEach(({ name }, index) => {
+      // This is being checked to determine that the correct catalog item is as the correct place in the table
+      cy.get(
+        `[data-cy="items-table"] [data-cy="row"]:nth-child(${
+          index + 1
+        }) [data-cy="name"]`
+      ).should('have.text', name)
+
+      cy.get(
+        `[data-cy="items-table"] [data-cy="row"]:nth-child(${
+          index + 1
+        }) [data-cy="quantity"]`
+      ).should('have.text', 1 + index)
+    })
   })
 })
