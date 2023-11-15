b31aade802b98a94ec83ce9a1909210bc9d8d1b1
Felix <jonfelixrico@gmail.com>
2023-11-15 22:27:58 +0800

    Revert "Create serial no component"
    This reverts commit 5045f9dc7fe5d19c32352c530b621df50cfcf1b5.

--- src/components/billing/BillingSerialNo.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '@chakra-ui/react'
-
-export default function BillingSerialNo({ serialNo }: { serialNo: number }) {
-  return (
-    <Text fontSize="xl" fontWeight="bold">
-      No. <span data-cy="serial-no">{String(serialNo).padStart(4, '0')}</span>
-    </Text>
-  )
-}
