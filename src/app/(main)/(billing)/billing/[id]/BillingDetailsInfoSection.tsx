f5be9b0d75b87239221161740bfeafdbaea023c1
Felix <jonfelixrico@gmail.com>
2023-11-15 22:34:39 +0800

    Add serial no

--- src/app/(main)/(billing)/billing/[id]/BillingDetailsInfoSection.tsx
+++ src/app/(main)/(billing)/billing/[id]/BillingDetailsInfoSection.tsx
@@ -1,5 +1,5 @@
 import { SavedBilling } from '@/types/SavedBilling'
-import { Flex, Text, Textarea } from '@chakra-ui/react'
+import { Box, Flex, Text, Textarea } from '@chakra-ui/react'
 
 export default function BillingDetailsInfoSection({
   billing,
@@ -8,6 +8,15 @@ export default function BillingDetailsInfoSection({
 }) {
   return (
     <>
+      <Flex align="center" justify="end" gap={1}>
+        <Text fontSize="sm" fontWeight="medium">
+          No.
+        </Text>
+        <Text fontSize="lg" fontWeight="medium" data-cy="serial-no">
+          {String(billing.serialNo).padStart(4, '0')}
+        </Text>
+      </Flex>
+
       <Flex direction="column" gap={2}>
         <Text fontWeight="bold">Notes</Text>
         <Textarea isReadOnly resize="none" value={billing.notes} />
