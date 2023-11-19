659396d0ac84bae41f972e7a6eb6cb8171407111
Felix <jonfelixrico@gmail.com>
2023-11-20 02:54:29 +0800

    Use server actions for logout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,15 +2,23 @@ import LogoutButton from '@/components/main-layout/LogoutButton'
 import MainLayout from '@/components/main-layout/MainLayout'
 import RouteLinks from '@/components/main-layout/RouteLinks'
 import { Container, Flex, Spacer } from '@chakra-ui/react'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
+  function logOut() {
+    'use server'
+    cookies().delete('auth')
+    redirect('/login')
+  }
+
   const drawer = (
     <Flex height="full" direction="column">
       <RouteLinks />
       <Spacer />
 
-      <LogoutButton />
+      <LogoutButton onLogOutConfirm={logOut} />
     </Flex>
   )
 
