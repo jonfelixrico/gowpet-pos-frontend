f8b6ca7451051c84672bff279d429d7f40d9dc66
Felix <jonfelixrico@gmail.com>
2023-11-20 00:44:30 +0800

    Integrate into layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,11 @@
+import DrawerRoutes from '@/components/main-layout/DrawerRoutes'
 import MainLayout from '@/components/main-layout/MainLayout'
 import { Container } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
   return (
-    <MainLayout height="100dvh">
+    <MainLayout height="100dvh" drawer={<DrawerRoutes />}>
       <Container maxW="container.md" height="100%">
         {children}
       </Container>
