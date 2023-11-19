96a0aca30644f9a90940a16ab25371bfcaa35ff8
Felix <jonfelixrico@gmail.com>
2023-11-19 23:30:38 +0800

    Integrate into layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,13 @@
+import MainLayout from '@/components/main-layout/MainLayout'
 import { Container } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
   return (
-    <Container maxW="container.md" height="100dvh">
-      {children}
-    </Container>
+    <MainLayout>
+      <Container maxW="container.md" height="100dvh">
+        {children}
+      </Container>
+    </MainLayout>
   )
 }
