9081280eb808556a490dd23386874c744570627e
Felix <jonfelixrico@gmail.com>
2023-11-20 00:56:52 +0800

    Relocate to its own component

--- src/components/main-layout/DrawerContent.tsx
+++ src/components/main-layout/DrawerContent.tsx
@@ -1,14 +1,14 @@
-import { Button, Flex, FlexProps, Spacer } from '@chakra-ui/react'
+import { Flex, FlexProps, Spacer } from '@chakra-ui/react'
 import RouteLinks from './RouteLinks'
+import LogoutButton from './LogoutButton'
 
 export default function DrawerContent(flexProps: FlexProps) {
   return (
     <Flex {...flexProps} direction="column">
       <RouteLinks />
       <Spacer />
-      <Button variant="ghost" colorScheme="red">
-        Log out
-      </Button>
+
+      <LogoutButton />
     </Flex>
   )
 }
---
+++ src/components/main-layout/LogoutButton.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from '@chakra-ui/react'
+
+export default function LogoutButton() {
+  return (
+    <>
+      <Button variant="ghost" colorScheme="red">
+        Log out
+      </Button>
+    </>
+  )
+}
