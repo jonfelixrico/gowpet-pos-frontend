89520febc740cf6c85f72f7bba9b62fac582bdcd
Felix <jonfelixrico@gmail.com>
2023-11-20 02:49:01 +0800

    Directly move the drawer

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,21 @@
-import DrawerContent from '@/components/main-layout/DrawerContent'
+import LogoutButton from '@/components/main-layout/LogoutButton'
 import MainLayout from '@/components/main-layout/MainLayout'
-import { Container } from '@chakra-ui/react'
+import RouteLinks from '@/components/main-layout/RouteLinks'
+import { Container, Flex, Spacer } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
+  const drawer = (
+    <Flex height="full" direction="column">
+      <RouteLinks />
+      <Spacer />
+
+      <LogoutButton />
+    </Flex>
+  )
+
   return (
-    <MainLayout height="100dvh" drawer={<DrawerContent height="full" />}>
+    <MainLayout height="100dvh" drawer={drawer}>
       <Container maxW="container.md" height="100%">
         {children}
       </Container>
