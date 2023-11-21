fde613ebb7fbb85089eb6fdb885a63c40a5425cc
Felix <jonfelixrico@gmail.com>
2023-11-22 01:22:06 +0800

    Create billing utils

---
+++ src/utils/billing-utils.tsx
@@ -0,0 +1,16 @@
+import { SavedBilling } from '@/types/SavedBilling'
+import EscPosEncoder from 'esc-pos-encoder'
+
+export function encodeForThermalReceipt({ items, serialNo }: SavedBilling) {
+  const encoder = new EscPosEncoder().initialize()
+
+  encoder.line(`Receipt no. ${serialNo}`)
+
+  for (const { catalogItem, price, quantity } of items) {
+    encoder.line(
+      `${catalogItem.name} - ${price} x ${quantity} = ${price * quantity}`
+    )
+  }
+
+  return encoder.encode()
+}
