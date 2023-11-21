c16e4e6e58fd4806f8b2db8f7722b0c70597cc04
Felix <jonfelixrico@gmail.com>
2023-11-22 01:01:57 +0800

    Create the billing receipt

---
+++ src/utils/billing-receipt-utils.ts
@@ -0,0 +1,22 @@
+import { SavedBilling } from '@/types/SavedBilling'
+import EscPosEncoder from 'esc-pos-encoder'
+import { sendToThermalPrinter } from './thermal-printer-bt-utils'
+
+function encodeReceiptContent({ items, serialNo }: SavedBilling) {
+  const encoder = new EscPosEncoder().initialize()
+
+  encoder.line(`Receipt no. ${serialNo}`)
+
+  for (const { catalogItem, price, quantity } of items) {
+    encoder.line(
+      `${catalogItem.name} - ${price} x ${quantity} = ${price * quantity}`
+    )
+  }
+
+  return encoder.encode()
+}
+
+export async function printReceiptViaThermalPrinter(billing: SavedBilling) {
+  const content = encodeReceiptContent(billing)
+  await sendToThermalPrinter(content)
+}
