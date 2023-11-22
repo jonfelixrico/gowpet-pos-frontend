0bda8c0c528d4d01abe07b799e74ae22ffbb7386
Felix <jonfelixrico@gmail.com>
2023-11-22 20:48:51 +0800

    Make adjustments

--- src/utils/thermal-printer-bt-utils.ts
+++ src/utils/thermal-printer-bt-utils.ts
@@ -1,8 +1,9 @@
 'use client'
 
-async function requestPrinter() {
+async function requestPrintingDevice() {
   // from https://github.com/WebBluetoothCG/demos/blob/gh-pages/bluetooth-printer/index.html
 
+  console.debug('Requesting for printer...')
   const device = await navigator.bluetooth.requestDevice({
     filters: [
       {
@@ -11,13 +12,28 @@ async function requestPrinter() {
     ],
   })
 
-  console.log('Found %s', device.name)
+  console.log('Obtained printer %s', device.name)
 
+  return device
+}
+
+let printerPromise: Promise<BluetoothDevice> | null = null
+async function getPrintingDevice() {
+  if (!printerPromise) {
+    console.log('No printers found yet. Requesting...')
+    printerPromise = requestPrintingDevice()
+  }
+
+  return await printerPromise
+}
+
+async function preparePrinter(device: BluetoothDevice) {
   if (!device.gatt) {
     throw new Error('No GATT server found')
   }
 
   console.log('Connecting to GATT server...')
+
   const gattServer = await device.gatt.connect()
   const service = await gattServer.getPrimaryService(
     '000018f0-0000-1000-8000-00805f9b34fb'
@@ -26,26 +42,17 @@ async function requestPrinter() {
     '00002af1-0000-1000-8000-00805f9b34fb'
   )
 
-  console.log('Finished setting up the thermal printer')
+  console.log('Finished preparing the printer')
 
   return characteristic
 }
 
-let printerPromise: Promise<BluetoothRemoteGATTCharacteristic> | null = null
-async function getPrinter() {
-  if (!printerPromise) {
-    console.log('No printers found yet. Requesting...')
-    printerPromise = requestPrinter()
-  }
-
-  return await printerPromise
-}
-
 export async function sendToThermalPrinter(toSend: Uint8Array) {
-  const printer = await getPrinter()
-  if (!printer) {
+  const device = await getPrintingDevice()
+  if (!device) {
     throw new Error('No printer found!')
   }
+  const printer = await preparePrinter(device)
 
   let index = 0
   while (index + 512 < toSend.length) {
