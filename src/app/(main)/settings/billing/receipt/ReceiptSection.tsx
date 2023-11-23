46210d88d1d9954ea92fdd510f19a64ec382a250
Felix <jonfelixrico@gmail.com>
2023-11-24 00:40:15 +0800

    Integrate

--- src/app/(main)/settings/billing/receipt/ReceiptSection.tsx
+++ src/app/(main)/settings/billing/receipt/ReceiptSection.tsx
@@ -1,6 +1,32 @@
+import If from '@/components/common/If'
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import { ReceiptSettings } from '@/types/ReceiptSetings'
 import { Divider, Flex, Text } from '@chakra-ui/react'
+import ReceiptSettingsUpdate from './ReceiptSettingsUpdate'
+import ReceiptSettingsCreate from './ReceiptSettingsCreate'
 
-export default function ReceiptSection() {
+export default async function ReceiptSection() {
+  const { data } = await apiFetchData<ReceiptSettings>('/billing/receipt')
+
+  async function save(settings: ReceiptSettings) {
+    'use server'
+
+    await apiFetchData('/billing/receipt', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(settings),
+    })
+  }
+
+  async function clear() {
+    'use server'
+    await apiFetchData('/billing/receipt', {
+      method: 'DELETE',
+    })
+  }
+
   return (
     <Flex gap={2} direction="column">
       <Text fontWeight="medium" fontSize="lg">
@@ -8,6 +34,14 @@ export default function ReceiptSection() {
       </Text>
 
       <Divider />
+
+      <If condition={!!data}>
+        <ReceiptSettingsUpdate clear={clear} save={save} settings={data} />
+      </If>
+
+      <If condition={!data}>
+        <ReceiptSettingsCreate save={save} />
+      </If>
     </Flex>
   )
 }
