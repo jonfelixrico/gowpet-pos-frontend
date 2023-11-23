5073e97cd4a60aeafd32092eb31eacfac8daa0f8
Felix <jonfelixrico@gmail.com>
2023-11-24 00:45:47 +0800

    Adjust spacing

--- src/app/(main)/settings/billing/receipt/ReceiptSettingsForm.tsx
+++ src/app/(main)/settings/billing/receipt/ReceiptSettingsForm.tsx
@@ -2,7 +2,7 @@
 
 import { ReceiptSettings } from '@/types/ReceiptSetings'
 import { FormikSubmit } from '@/types/formik'
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Button, Flex, FormControl, FormLabel, Input } from '@chakra-ui/react'
 import { Field, FieldProps, Form, Formik } from 'formik'
 
 export default function ReceiptSettingsForm({
@@ -30,59 +30,61 @@ export default function ReceiptSettingsForm({
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
       {(props) => (
         <Form>
-          <Field name="header">
-            {({ field }: FieldProps) => (
-              <FormControl>
-                <FormLabel>Header</FormLabel>
-                <Input {...field} />
-              </FormControl>
-            )}
-          </Field>
+          <Flex direction="column" gap={3}>
+            <Field name="header">
+              {({ field }: FieldProps) => (
+                <FormControl>
+                  <FormLabel>Header</FormLabel>
+                  <Input {...field} />
+                </FormControl>
+              )}
+            </Field>
 
-          <Field name="address">
-            {({ field }: FieldProps) => (
-              <FormControl>
-                <FormLabel>Address</FormLabel>
-                <Input {...field} />
-              </FormControl>
-            )}
-          </Field>
+            <Field name="address">
+              {({ field }: FieldProps) => (
+                <FormControl>
+                  <FormLabel>Address</FormLabel>
+                  <Input {...field} />
+                </FormControl>
+              )}
+            </Field>
 
-          <Field name="contactNo">
-            {({ field }: FieldProps) => (
-              <FormControl>
-                <FormLabel>Contact Information</FormLabel>
-                <Input {...field} />
-              </FormControl>
-            )}
-          </Field>
+            <Field name="contactNo">
+              {({ field }: FieldProps) => (
+                <FormControl>
+                  <FormLabel>Contact information</FormLabel>
+                  <Input {...field} />
+                </FormControl>
+              )}
+            </Field>
 
-          <Field name="snsLink">
-            {({ field }: FieldProps) => (
-              <FormControl>
-                <FormLabel>Social Media URL</FormLabel>
-                <Input {...field} />
-              </FormControl>
-            )}
-          </Field>
+            <Field name="snsLink">
+              {({ field }: FieldProps) => (
+                <FormControl>
+                  <FormLabel>Social media URL</FormLabel>
+                  <Input {...field} />
+                </FormControl>
+              )}
+            </Field>
 
-          <Field name="snsMessage">
-            {({ field }: FieldProps) => (
-              <FormControl>
-                <FormLabel>Social Media Message</FormLabel>
-                <Input {...field} />
-              </FormControl>
-            )}
-          </Field>
+            <Field name="snsMessage">
+              {({ field }: FieldProps) => (
+                <FormControl>
+                  <FormLabel>Social media message</FormLabel>
+                  <Input {...field} />
+                </FormControl>
+              )}
+            </Field>
 
-          <Button
-            mt={4}
-            colorScheme="blue"
-            isLoading={props.isSubmitting}
-            type="submit"
-          >
-            Save Settings
-          </Button>
+            <Button
+              mt={4}
+              colorScheme="blue"
+              isLoading={props.isSubmitting}
+              type="submit"
+            >
+              Save settings
+            </Button>
+          </Flex>
         </Form>
       )}
     </Formik>
