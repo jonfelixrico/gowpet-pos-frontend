65f33b8374969e5b5195d9b14e12930907193c1c
Felix <jonfelixrico@gmail.com>
2023-11-23 23:23:14 +0800

    Improve the layout

--- src/app/(main)/settings/layout.tsx
+++ src/app/(main)/settings/layout.tsx
@@ -1,12 +1,18 @@
-import { Card, CardBody, Flex } from '@chakra-ui/react'
+import { Card, CardBody, Container, Flex } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function SettingsLayout({ children }: { children: ReactNode }) {
   return (
-    <Flex height="full" gap={2} direction="column">
+    <Container
+      maxW="container.md"
+      height="full"
+      as={Flex}
+      direction="column"
+      padding={2}
+    >
       <Card flex={1}>
         <CardBody>{children}</CardBody>
       </Card>
-    </Flex>
+    </Container>
   )
 }
