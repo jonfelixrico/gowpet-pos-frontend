79fa64e1000e0437f4014f9cc56ad3da3695a1b6
Felix <jonfelixrico@gmail.com>
2023-11-29 22:08:44 +0800

    Improve tests

--- cypress/components/contexts/OffscreenContainerContext.cy.tsx
+++ cypress/components/contexts/OffscreenContainerContext.cy.tsx
@@ -4,25 +4,34 @@ import {
 } from '@/contexts/OffscreenContainerContext'
 
 describe('OffscreenContainerContext', () => {
-  it('renders its children', () => {
-    cy.mount(
-      <OffscreenContainerProvider>
-        <div data-cy="child" />
-      </OffscreenContainerProvider>
-    )
-
-    cy.dataCy('child').should('exist')
-  })
-
   it('renders via portal', () => {
     cy.mount(
-      <OffscreenContainerProvider>
-        <OffscreenContainerPortal>
-          <div data-cy="thru-portal" />
-        </OffscreenContainerPortal>
-      </OffscreenContainerProvider>
+      <div data-cy="test-root">
+        <OffscreenContainerProvider data-cy="portal">
+          <div data-cy="1">
+            <div data-cy="2">
+              <div data-cy="3">
+                <div data-cy="inner-content" />
+                <OffscreenContainerPortal>
+                  <div data-cy="thru-portal" />
+                </OffscreenContainerPortal>
+              </div>
+            </div>
+          </div>
+        </OffscreenContainerProvider>
+      </div>
     )
 
-    cy.dataCy('thru-portal').should('exist')
+    cy.get(
+      '[data-cy="test-root"] [data-cy="portal"] [data-cy="thru-portal"]'
+    ).should('exist')
+    cy.get(
+      '[data-cy="1"] [data-cy="2"] [data-cy="3"] [data-cy="inner-content"]'
+    ).should('exist')
+
+    // Shouldn't be a sibling of the inner content. It should be rendered via a portal
+    cy.get(
+      '[data-cy="1"] [data-cy="2"] [data-cy="3"] [data-cy="thru-portal"]'
+    ).should('not.exist')
   })
 })
