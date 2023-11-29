d78e0dc754909da6eb5d5cf754bcd1cbcf4d25ab
Felix <jonfelixrico@gmail.com>
2023-11-29 22:09:47 +0800

    Tweak hte tests for the print receipt button

--- cypress/components/billing/receipt/BillingPrintReceiptButton.cy.tsx
+++ cypress/components/billing/receipt/BillingPrintReceiptButton.cy.tsx
@@ -1,17 +1,20 @@
 import BillingPrintReceiptButton from '@/app/(main)/(billing)/billing/[id]/receipt/BillingPrintReceiptButton'
+import { OffscreenContainerProvider } from '@/contexts/OffscreenContainerContext'
 import { ChakraProvider } from '@chakra-ui/react'
 
 describe('BillingPrintReceiptButton', () => {
   it('is disabled if receiptSettings is nullish', () => {
     cy.mount(
       <ChakraProvider>
-        <BillingPrintReceiptButton
-          billing={{
-            id: 'random id',
-            items: [],
-            serialNo: 1,
-          }}
-        />
+        <OffscreenContainerProvider>
+          <BillingPrintReceiptButton
+            billing={{
+              id: 'random id',
+              items: [],
+              serialNo: 1,
+            }}
+          />
+        </OffscreenContainerProvider>
       </ChakraProvider>
     )
 
@@ -21,20 +24,22 @@ describe('BillingPrintReceiptButton', () => {
   it('is enabled if receiptSettings is not nullish', () => {
     cy.mount(
       <ChakraProvider>
-        <BillingPrintReceiptButton
-          billing={{
-            id: 'random id',
-            items: [],
-            serialNo: 1,
-          }}
-          receiptSettings={{
-            address: 'test address',
-            contactNo: 'test contact no',
-            header: 'test header',
-            snsLink: 'http://test-sns',
-            snsMessage: 'test sns message',
-          }}
-        />
+        <OffscreenContainerProvider>
+          <BillingPrintReceiptButton
+            billing={{
+              id: 'random id',
+              items: [],
+              serialNo: 1,
+            }}
+            receiptSettings={{
+              address: 'test address',
+              contactNo: 'test contact no',
+              header: 'test header',
+              snsLink: 'http://test-sns',
+              snsMessage: 'test sns message',
+            }}
+          />
+        </OffscreenContainerProvider>
       </ChakraProvider>
     )
 
