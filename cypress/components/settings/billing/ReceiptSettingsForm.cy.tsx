e2aa25df4780d746ffb0f1342b48dfce6240a2d1
Felix <jonfelixrico@gmail.com>
2023-11-30 21:52:56 +0800

    Add more tests

--- cypress/components/settings/billing/ReceiptSettingsForm.cy.tsx
+++ cypress/components/settings/billing/ReceiptSettingsForm.cy.tsx
@@ -1,5 +1,6 @@
 import ReceiptSettingsForm from '@/app/(main)/settings/billing/receipt/ReceiptSettingsForm'
 import { ReceiptSettings } from '@/types/ReceiptSetings'
+import { EMPTY_FN } from '@/utils/misc-utills'
 import { ChakraProvider } from '@chakra-ui/react'
 
 describe('ReceiptSettingsForm', () => {
@@ -29,5 +30,38 @@ describe('ReceiptSettingsForm', () => {
       snsMessage: 'Follow us on Facebook!',
       snsLink: 'http://facebook.com',
     } as ReceiptSettings)
+  })
+
+  it('should show initial state', () => {
+    cy.mount(
+      <ChakraProvider>
+        <ReceiptSettingsForm
+          onSubmit={EMPTY_FN}
+          initialValues={{
+            address:
+              'Malacañang Complex. J.P. Laurel Street, San Miguel,. 1005 Manila City',
+            header: 'Business, Inc.',
+            contactNo: '0920-123-4567',
+            snsMessage: 'Follow us on Facebook!',
+            snsLink: 'http://facebook.com',
+          }}
+        />
+      </ChakraProvider>
+    )
+
+    cy.dataCy('header').find('input').should('have.value', 'Business, Inc.')
+    cy.dataCy('contact-no').find('input').should('have.value', '0920-123-4567')
+    cy.dataCy('address')
+      .find('input')
+      .should(
+        'have.value',
+        'Malacañang Complex. J.P. Laurel Street, San Miguel,. 1005 Manila City'
+      )
+    cy.dataCy('sns-link')
+      .find('input')
+      .should('have.value', 'http://facebook.com')
+    cy.dataCy('sns-message')
+      .find('input')
+      .should('have.value', 'Follow us on Facebook!')
   })
 })
