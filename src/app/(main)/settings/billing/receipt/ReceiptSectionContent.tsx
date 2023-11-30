8114ae608bc6fe1446469a692287a7b59d5c0587
Felix <jonfelixrico@gmail.com>
2023-11-30 21:38:02 +0800

    Create content component

---
+++ src/app/(main)/settings/billing/receipt/ReceiptSectionContent.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { Button, Flex } from '@chakra-ui/react'
+import { startTransition, useState } from 'react'
+import ReceiptSettingsForm, { OnSubmitFunction } from './ReceiptSettingsForm'
+import { ReceiptSettings } from '@/types/ReceiptSetings'
+
+function ReceiptSettingsCreate({ onSave }: { onSave: OnSubmitFunction }) {
+  const [showForm, setShowForm] = useState(false)
+
+  if (!showForm) {
+    return (
+      <Button colorScheme="blue" onClick={() => setShowForm(true)}>
+        Set-up receipt
+      </Button>
+    )
+  }
+
+  return (
+    <Flex direction="column" gap={2}>
+      <ReceiptSettingsForm
+        initialValues={{
+          address: '',
+          contactNo: '',
+          header: '',
+          snsLink: '',
+          snsMessage: '',
+        }}
+        onSubmit={onSave}
+      />
+
+      <Button
+        onClick={() => setShowForm(false)}
+        variant="ghost"
+        colorScheme="red"
+      >
+        Abort receipt set-up
+      </Button>
+    </Flex>
+  )
+}
+
+function ReceiptSettingsUpdate({
+  onSave,
+  onClear,
+  settings,
+}: {
+  onSave: OnSubmitFunction
+  onClear: () => Promise<void>
+  settings: ReceiptSettings
+}) {
+  function triggerClear() {
+    startTransition(() => {
+      onClear()
+    })
+  }
+
+  return (
+    <Flex direction="column" gap={2}>
+      <ReceiptSettingsForm initialValues={settings} onSubmit={onSave} />
+
+      <Button onClick={triggerClear} variant="ghost" colorScheme="red">
+        Clear receipt settings
+      </Button>
+    </Flex>
+  )
+}
+
+export default function ReceiptSectionContent({
+  onSave,
+  onClear,
+  settings,
+}: {
+  onSave: OnSubmitFunction
+  onClear: () => Promise<void>
+  settings: ReceiptSettings
+}) {
+  if (!settings) {
+    return <ReceiptSettingsCreate onSave={onSave} />
+  }
+
+  return (
+    <ReceiptSettingsUpdate
+      onClear={onClear}
+      onSave={onSave}
+      settings={settings}
+    />
+  )
+}
