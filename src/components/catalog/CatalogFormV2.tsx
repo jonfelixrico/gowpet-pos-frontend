5fcccc4152528057b689d9b90281fb0a4cfd4fc6
Felix <jonfelixrico@gmail.com>
2023-12-01 23:45:30 +0800

    Wrap onSubmit

--- src/components/catalog/CatalogFormV2.tsx
+++ src/components/catalog/CatalogFormV2.tsx
@@ -17,6 +17,7 @@ export interface CatalogFormFields {
   name: string
   price: number
 }
+export type CatalogFormSubmitFn = (value: CatalogFormFields) => Promise<void>
 
 export default function CatalogForm({
   handleSubmit,
@@ -25,11 +26,19 @@ export default function CatalogForm({
     price: 0,
   },
 }: {
-  handleSubmit: FormikSubmit<CatalogFormFields>
+  handleSubmit: CatalogFormSubmitFn
   initialValues?: CatalogFormFields
 }) {
+  const onSubmit: FormikSubmit<CatalogFormFields> = async (values, actions) => {
+    try {
+      await handleSubmit(values)
+    } finally {
+      actions.setSubmitting(false)
+    }
+  }
+
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
       {(props) => (
         <Form data-cy="form">
           <Flex direction="column" alignItems="stretch" gap={2}>
