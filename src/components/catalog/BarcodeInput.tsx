80df87cde97b899890474efbd7a08360e1fe43ed
Felix <jonfelixrico@gmail.com>
2023-12-02 02:02:43 +0800

    Add more func to BarcodeInput

--- src/components/catalog/BarcodeInput.tsx
+++ src/components/catalog/BarcodeInput.tsx
@@ -14,21 +14,25 @@ import {
   ModalHeader,
   ModalOverlay,
   ModalProps,
+  useDisclosure,
 } from '@chakra-ui/react'
 import If from '../common/If'
 import { useZxing } from 'react-zxing'
+import { useRef, useState } from 'react'
 
 function BarcodeScannerModal({
   isOpen,
   onClose,
   onScan,
   ...props
-}: ModalProps & {
+}: Omit<ModalProps, 'children'> & {
   onScan: (code: string) => void
 }) {
+  const [result, setResult] = useState('')
+
   const { ref } = useZxing({
     onDecodeResult(result) {
-      onScan(result.getText())
+      setResult(result.getText())
     },
   })
 
@@ -45,8 +49,16 @@ function BarcodeScannerModal({
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
+          <Button variant="ghost" mr={3} onClick={onClose}>
             Close
+          </Button>
+
+          <Button
+            colorScheme="blue"
+            onClick={() => onScan(result)}
+            isDisabled={!!result}
+          >
+            Submit
           </Button>
         </ModalFooter>
       </ModalContent>
@@ -54,11 +66,35 @@ function BarcodeScannerModal({
   )
 }
 
-export default function BarcodeInput({ onInput, ...props }: InputProps) {
+export default function BarcodeInput(props: InputProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const ref = useRef<HTMLInputElement | null>(null)
+
+  function propagateAsChangeEvent(code: string) {
+    const el = ref.current
+    if (!el) {
+      // TODO remove this
+      console.debug('el not found')
+      return
+    }
+
+    el.value = code
+    el.dispatchEvent(new Event('change'))
+  }
+
   return (
     <InputGroup>
-      <Input {...props} onInput={onInput} />
-      <InputRightElement></InputRightElement>
+      <Input {...props} ref={ref} />
+      <InputRightElement>
+        <Button onClick={onOpen}>Use Scanner</Button>
+
+        <BarcodeScannerModal
+          isOpen={isOpen}
+          onClose={onClose}
+          onScan={propagateAsChangeEvent}
+        />
+      </InputRightElement>
     </InputGroup>
   )
 }
