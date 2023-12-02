5463bcb423bd76d533bb56569f96ec26f07e0041
Felix <jonfelixrico@gmail.com>
2023-12-02 20:23:40 +0800

    Integrate the new BarcodeScannerModal

--- src/components/barcode/BarcodeInput.tsx
+++ src/components/barcode/BarcodeInput.tsx
@@ -6,65 +6,10 @@ import {
   InputGroup,
   InputProps,
   InputRightElement,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  ModalProps,
   useDisclosure,
 } from '@chakra-ui/react'
-import { useRef, useState } from 'react'
-import BarcodeScanner from './BarcodeScanner'
-import { DetectionResults } from './BarcodeCamera'
-import If from '@/components/common/If'
-
-function BarcodeScannerModal({
-  isOpen,
-  onClose,
-  onScan,
-  ...props
-}: Omit<ModalProps, 'children'> & {
-  onScan: (code: string) => void
-}) {
-  const [result, setResult] = useState('')
-
-  function onBarcodeDetect({ barcodes, image }: DetectionResults) {
-    const { rawValue } = barcodes[0]
-    setResult(rawValue)
-  }
-
-  return (
-    <Modal {...props} isOpen={isOpen} onClose={onClose} scrollBehavior="inside">
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Barcode/QR Scanner</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <If condition={isOpen}>
-            <BarcodeScanner onDetect={onBarcodeDetect} height="50dvh" />
-          </If>
-        </ModalBody>
-
-        <ModalFooter>
-          <Button variant="ghost" mr={3} onClick={onClose}>
-            Close
-          </Button>
-
-          <Button
-            colorScheme="blue"
-            onClick={() => onScan(result)}
-            isDisabled={!result}
-          >
-            Submit
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-  )
-}
+import { useRef } from 'react'
+import BarcodeScannerModal from './BarcodeScannerModal'
 
 export default function BarcodeInput(props: InputProps) {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -92,7 +37,7 @@ export default function BarcodeInput(props: InputProps) {
         <BarcodeScannerModal
           isOpen={isOpen}
           onClose={onClose}
-          onScan={propagateAsChangeEvent}
+          onSubmit={propagateAsChangeEvent}
         />
       </InputRightElement>
     </InputGroup>
