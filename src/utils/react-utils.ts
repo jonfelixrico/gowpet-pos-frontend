2e9503855301715eca49fa54dcd68b38c5be2ceb
Felix <jonfelixrico@gmail.com>
2023-12-03 17:52:36 +0800

    Add comments

--- src/utils/react-utils.ts
+++ src/utils/react-utils.ts
@@ -1,3 +1,11 @@
+/**
+ * WARNING: HACKY!
+ *
+ * This makes an input element emit a value. This approach is something specific
+ * to our React version (>= 15.6)
+ *
+ * See https://stackoverflow.com/a/46012210
+ */
 export function makeInputEmitInputEvent<T>(input: HTMLInputElement, value: T) {
   const proto = window.HTMLInputElement.prototype
   if (!proto) {
