c1bcbcd86390445b46a7e01877ce41923b7f7d58
Felix <jonfelixrico@gmail.com>
2023-12-05 02:13:11 +0800

    Implement useScan

--- src/app/(main)/(billing)/billing/create/scan/BarcodeScannerButton.tsx
+++ src/app/(main)/(billing)/billing/create/scan/BarcodeScannerButton.tsx
@@ -1,5 +1,8 @@
+import { DetectionResults } from '@/components/barcode/BarcodeScanner'
 import BarcodeScannerControls from '@/components/barcode/BarcodeScannerControls'
 import { Billing } from '@/types/Billing'
+import { CatalogItem } from '@/types/CatalogItem'
+import { fetchData } from '@/utils/fetch-utils'
 import {
   Button,
   ButtonProps,
@@ -12,12 +15,82 @@ import {
   ModalOverlay,
   useDisclosure,
 } from '@chakra-ui/react'
-import { Dispatch, SetStateAction, useMemo } from 'react'
+import { produce } from 'immer'
+import { Dispatch, SetStateAction, useMemo, useState } from 'react'
 import { If, Then } from 'react-if'
 
 interface BillingStateProps {
   billing: Billing
   setBilling: Dispatch<SetStateAction<Billing>>
+}
+
+function useScan({ billing, setBilling }: BillingStateProps) {
+  const { items } = billing
+
+  const codes = useMemo(
+    () => items.map((item) => item.catalogItem.code).filter(Boolean),
+    [items]
+  )
+  const codesSet = useMemo(() => new Set(codes), [codes])
+
+  const [loadingCodes, setLoadingCodes] = useState<Record<string, boolean>>({})
+
+  async function detect({ rawValue }: DetectedBarcode) {
+    if (codesSet.has(rawValue) || loadingCodes[rawValue]) {
+      return
+    }
+
+    try {
+      setLoadingCodes((codes) =>
+        produce(codes, (codes) => {
+          codes[rawValue] = true
+        })
+      )
+
+      const { data } = await fetchData<CatalogItem>(
+        `/billing/catalog/code/${rawValue}`
+      )
+
+      setBilling((billing) =>
+        produce(billing, (billing) => {
+          billing.items.push({
+            catalogItem: data,
+            quantity: 1,
+          })
+        })
+      )
+    } catch (e) {
+      // TODO add something
+    } finally {
+      setLoadingCodes((codes) =>
+        produce(codes, (codes) => {
+          delete codes[rawValue]
+        })
+      )
+    }
+  }
+
+  function onDetect({ barcodes }: DetectionResults) {
+    Promise.all(barcodes.map(detect))
+  }
+
+  const barcodeColors = useMemo(() => {
+    return codes.reduce(
+      (map, code) => {
+        if (code) {
+          map[code] = 'gray'
+        }
+
+        return map
+      },
+      {} as Record<string, string>
+    )
+  }, [codes])
+
+  return {
+    barcodeColors,
+    onDetect,
+  }
 }
 
 export default function BarcodeScannerButton({
@@ -26,6 +99,8 @@ export default function BarcodeScannerButton({
   ...buttonProps
 }: BillingStateProps & ButtonProps) {
   const { isOpen, onClose, onOpen } = useDisclosure()
+
+  const { onDetect } = useScan({ billing, setBilling })
 
   return (
     <>
@@ -36,7 +111,9 @@ export default function BarcodeScannerButton({
           <ModalCloseButton />
           <ModalBody>
             <If condition={isOpen}>
-              <Then>{/* <BarcodeScannerControls /> */}</Then>
+              <Then>
+                <BarcodeScannerControls onDetect={onDetect} />
+              </Then>
             </If>
           </ModalBody>
 
