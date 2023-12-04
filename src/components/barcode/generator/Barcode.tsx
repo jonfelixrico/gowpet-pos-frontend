1088678d7ac33826141f8e3f692b33085a3ec04e
Felix <jonfelixrico@gmail.com>
2023-12-05 00:44:12 +0800

    Adjust barcode settings

--- src/components/barcode/generator/Barcode.tsx
+++ src/components/barcode/generator/Barcode.tsx
@@ -16,7 +16,13 @@ export default function Barcode({
 
   useEffect(() => {
     const canvas = document.createElement('canvas')
-    JsBarcode(canvas, value, { displayValue: false, margin: 0, ...options })
+    JsBarcode(canvas, value, {
+      displayValue: false,
+      width: 10 * 2,
+      height: 10 * 100,
+      margin: 0,
+      ...options,
+    })
     setUrl(canvas.toDataURL())
   }, [value, options])
 
