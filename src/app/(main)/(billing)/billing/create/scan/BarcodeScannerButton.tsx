c2dfd4c194faba1d6df9281afd323ab7df77caf9
Felix <jonfelixrico@gmail.com>
2023-12-05 01:43:53 +0800

    Implement modal

--- src/app/(main)/(billing)/billing/create/scan/BarcodeScannerButton.tsx
+++ src/app/(main)/(billing)/billing/create/scan/BarcodeScannerButton.tsx
@@ -1,6 +1,19 @@
+import BarcodeScannerControls from '@/components/barcode/BarcodeScannerControls'
 import { Billing } from '@/types/Billing'
-import { Button, ButtonProps } from '@chakra-ui/react'
-import { Dispatch, SetStateAction } from 'react'
+import {
+  Button,
+  ButtonProps,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from '@chakra-ui/react'
+import { Dispatch, SetStateAction, useMemo } from 'react'
+import { If, Then } from 'react-if'
 
 interface BillingStateProps {
   billing: Billing
@@ -12,9 +25,30 @@ export default function BarcodeScannerButton({
   setBilling,
   ...buttonProps
 }: BillingStateProps & ButtonProps) {
+  const { isOpen, onClose, onOpen } = useDisclosure()
+
   return (
     <>
-      <Button {...buttonProps} size="sm">
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Scanner</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <If condition={isOpen}>
+              <Then>{/* <BarcodeScannerControls /> */}</Then>
+            </If>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Button {...buttonProps} size="sm" onClick={onOpen}>
         Barcode Scanner
       </Button>
     </>
