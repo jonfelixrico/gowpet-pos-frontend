959026da74533b7a9519a9668f57445ccdfeac9f
Felix <jonfelixrico@gmail.com>
2023-12-04 23:24:39 +0800

    Create barcode component

---
+++ src/components/barcode/generator/Barcode.tsx
@@ -0,0 +1,22 @@
+import { Image, ImageProps } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import JsBarcode, { Options } from 'jsbarcode'
+
+export default function Barcode({
+  value,
+  options,
+  ...props
+}: ImageProps & {
+  value: string
+  options?: Options
+}) {
+  const [url, setUrl] = useState('')
+
+  useEffect(() => {
+    const canvas = document.createElement('canvas')
+    JsBarcode(canvas, value, options ?? {})
+    setUrl(canvas.toDataURL())
+  }, [value, options])
+
+  return <Image {...props} src={url} alt={`Barcode of ${value}`} />
+}
