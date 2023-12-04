a7580176a84499140ccc82b1841953323287eaf1
Felix <jonfelixrico@gmail.com>
2023-12-05 00:45:45 +0800

    Make responsive

--- src/app/(main)/catalog/[catalogId]/CatalogDetailsCard.tsx
+++ src/app/(main)/catalog/[catalogId]/CatalogDetailsCard.tsx
@@ -1,7 +1,7 @@
 import Barcode from '@/components/barcode/generator/Barcode'
 import QrCode from '@/components/barcode/generator/QrCode'
 import { CatalogItem } from '@/types/CatalogItem'
-import { Box, Card, CardBody, Flex, Heading, Text } from '@chakra-ui/react'
+import { Card, CardBody, Flex, Heading, Text } from '@chakra-ui/react'
 import { Else, If, Then } from 'react-if'
 
 export default function CatalogDetailsCard({
@@ -10,10 +10,18 @@ export default function CatalogDetailsCard({
   catalogItem: CatalogItem
 }) {
   const { name, price, code, codeType } = catalogItem
+
   return (
     <Card>
-      <CardBody>
-        <Flex direction="column" gap={2}>
+      <CardBody
+        as={Flex}
+        gap={2}
+        direction={{
+          base: 'column',
+          sm: 'row',
+        }}
+      >
+        <Flex direction="column" gap={2} flex={1}>
           <Flex gap={2}>
             <Heading size="sx">Name:</Heading>
             <Text data-cy="name">{name}</Text>
@@ -25,23 +33,34 @@ export default function CatalogDetailsCard({
             </Heading>
             <Text data-cy="price">{price}</Text>
           </Flex>
-
-          <If condition={code}>
-            <Then>
-              <Box>
-                <If condition={codeType === 'CUSTOM'}>
-                  <Then>
-                    <QrCode value={code as string} />
-                  </Then>
-
-                  <Else>
-                    <Barcode value={code as string} />
-                  </Else>
-                </If>
-              </Box>
-            </Then>
-          </If>
         </Flex>
+
+        <If condition={code}>
+          <Then>
+            <Flex
+              direction="column"
+              flex={{
+                base: 1,
+                sm: 0.3,
+              }}
+              gap={1}
+            >
+              <If condition={codeType === 'CUSTOM'}>
+                <Then>
+                  <QrCode value={code as string} />
+                </Then>
+
+                <Else>
+                  <Barcode value={code as string} />
+                </Else>
+              </If>
+
+              <Text fontSize="xs" textAlign="center">
+                {code as string}
+              </Text>
+            </Flex>
+          </Then>
+        </If>
       </CardBody>
     </Card>
   )
