44a6a9e91530c71b3e4f8978861cb4ed3bd67afd
Felix <jonfelixrico@gmail.com>
2023-12-05 22:49:47 +0800

    Adjust the hook

--- src/components/billing/create/scan/billing-barcode-scanner-hook.ts
+++ src/components/billing/create/scan/billing-barcode-scanner-hook.ts
@@ -6,6 +6,7 @@ import { mapValues } from 'lodash'
 import { useMemo } from 'react'
 import useFetchCode from './fetch-code-hook'
 import { UseStateOutput } from '@/types/react-types'
+import { useToast } from '@chakra-ui/react'
 
 export default function useBillingBarcodeScanner({
   state: [billing, setBilling],
@@ -18,8 +19,9 @@ export default function useBillingBarcodeScanner({
     [items]
   )
 
-  const { fetchCode, loadingMap } = useFetchCode()
+  const toast = useToast()
 
+  const { fetchCode, loadingMap } = useFetchCode()
   async function detect({ rawValue }: DetectedBarcode) {
     if (codes.has(rawValue) || loadingMap[rawValue]) {
       return
@@ -30,6 +32,10 @@ export default function useBillingBarcodeScanner({
       if (!data) {
         return
       }
+
+      toast({
+        title: `Scanned ${data.name}`,
+      })
 
       setBilling((billing) =>
         produce(billing, (billing) => {
