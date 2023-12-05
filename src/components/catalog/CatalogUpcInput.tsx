d29e5b99a0a72f7c26e3c70cbb0faff0b82c1fec
Felix <jonfelixrico@gmail.com>
2023-12-06 00:42:27 +0800

    Adjust the looks

--- src/components/catalog/CatalogUpcInput.tsx
+++ src/components/catalog/CatalogUpcInput.tsx
@@ -54,7 +54,12 @@ export default function CatalogUpcInput({ value, ...others }: InputProps) {
           </Then>
 
           <Else>
-            <IconButton onClick={onOpen} size="sm" aria-label="Scan barcode">
+            <IconButton
+              onClick={onOpen}
+              size="sm"
+              aria-label="Scan barcode"
+              variant="outline"
+            >
               <BsUpcScan />
             </IconButton>
 
