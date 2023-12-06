ac1f0b742d32933cf8a2808e079a8f6a462300ba
Felix <jonfelixrico@gmail.com>
2023-12-06 21:36:26 +0800

    Create authenticate method

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,9 @@ import { RedirectType, redirect } from 'next/navigation'
 import { getAuthToken } from '@/utils/auth-util'
 import { verifyToken } from '@/server-utils/jwt-utils'
 import { DEFAULT_ROUTE } from '@/app/default-route'
+import { apiFetch } from '@/server-utils/resource-api-util'
+import { cookies } from 'next/headers'
+import { FetchError } from '@/utils/fetch-utils'
 
 export default async function Login() {
   const authToken = getAuthToken()
@@ -13,6 +16,36 @@ export default async function Login() {
      * even though the user is already authenticated
      */
     redirect(DEFAULT_ROUTE, RedirectType.replace)
+  }
+
+  async function authenticate(credentials: {
+    username: string
+    password: string
+  }) {
+    'use server'
+
+    try {
+      // TODO server actions for this
+      const response = await apiFetch('/authenticate', {
+        method: 'POST',
+        body: JSON.stringify(credentials),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      cookies().set('token', await response.text())
+      redirect(DEFAULT_ROUTE)
+    } catch (e) {
+      if (e instanceof FetchError) {
+        redirect(
+          `/login?redirectError=${e.response.status}`,
+          RedirectType.replace
+        )
+      }
+
+      redirect('/login?redirectError=500', RedirectType.replace)
+    }
   }
 
   return (
