0e3e73feb225c6a68266449a9fa81003e63efaf5
Felix <jonfelixrico@gmail.com>
2023-12-07 00:46:24 +0800

    Add more test cases

--- cypress/e2e/session-invalidated.cy.ts
+++ cypress/e2e/session-invalidated.cy.ts
@@ -16,4 +16,21 @@ describe('session invalidated', () => {
 
     cy.location('pathname').should('not.equal', '/login')
   })
+
+  it('handles mid-session invalidation with manual navigation to login', () => {
+    // test path choice was arbitrary
+    cy.visit('/billing/create')
+    // just to smoke-test that the user has been logged in
+    cy.location('pathname').should('equal', '/billing/create')
+
+    cy.setCookie('token', 'now invalidated')
+    cy.visit('/login')
+    cy.location('pathname').should('equal', '/login')
+
+    cy.dataCy('username').type('root')
+    cy.dataCy('password').type('password')
+    cy.dataCy('submit').click()
+
+    cy.location('pathname').should('not.equal', '/login')
+  })
 })
