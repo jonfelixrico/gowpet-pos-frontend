43d94422a621d9f7f9013528b4109a509cc35541
Felix <jonfelixrico@gmail.com>
2023-12-07 01:18:47 +0800

    Adjust for a Next bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,30 +4,41 @@ import { RedirectType, redirect } from 'next/navigation'
 import { DEFAULT_ROUTE } from '@/app/default-route'
 import { apiFetch } from '@/server-utils/resource-api-util'
 import { cookies } from 'next/headers'
-import { FetchError } from '@/utils/fetch-utils'
+import { ApiResponse, FetchError } from '@/utils/fetch-utils'
 import { Credentials } from '@/types/login-types'
 
 export default async function Login() {
   async function authenticate(credentials: Credentials) {
     'use server'
 
-    try {
-      const response = await apiFetch('/authenticate', {
+    /*
+     * Can't use try-catch here because of this weird Next bug:
+     * https://github.com/vercel/next.js/issues/49298#issuecomment-1649261356
+     */
+
+    const result: Error | ApiResponse<undefined> = await apiFetch(
+      '/authenticate',
+      {
         method: 'POST',
         body: JSON.stringify(credentials),
         headers: {
           'Content-Type': 'application/json',
         },
-      })
-
-      cookies().set('token', await response.text())
-      redirect(DEFAULT_ROUTE)
-    } catch (e) {
-      if (e instanceof FetchError) {
-        redirect(`/login?authError=${e.response.status}`, RedirectType.replace)
       }
+    )
+      .then((res) => res)
+      .catch((err) => err)
 
+    if (result instanceof FetchError) {
+      redirect(
+        `/login?authError=${result.response.status}`,
+        RedirectType.replace
+      )
+    } else if (result instanceof Error) {
       redirect('/login?authError=500', RedirectType.replace)
+    } else {
+      cookies().set('token', await result.text())
+      redirect(DEFAULT_ROUTE)
     }
   }
 
