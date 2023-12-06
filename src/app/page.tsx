df8d98bff3e7c403f1906da667ea18e1dc179569
Felix <jonfelixrico@gmail.com>
2023-12-07 00:47:20 +0800

    Add replace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+import { RedirectType, redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/login')
+  redirect('/login', RedirectType.replace)
 }
