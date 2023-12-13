d700316c33e9613fa738cb36d4fa18916290501b
Felix <jonfelixrico@gmail.com>
2023-12-14 00:48:31 +0800

    Create tests for loginRedirect

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -14,4 +14,21 @@ describe('login', () => {
     cy.visit('/login')
     cy.location('pathname').should('not.equal', '/login')
   })
+
+  it('redirects to initial page on login', () => {
+    cy.setCookie('token', '')
+
+    cy.visit('/catalog?pageNo=1&searchTerm=test')
+    cy.location('pathname').should('equal', '/login')
+
+    cy.get('[data-cy="username"]').type('root')
+    cy.get('[data-cy="password"]').type('password')
+    cy.get('[data-cy="submit"]').click()
+
+    cy.location('pathname').should('equal', '/catalog')
+
+    // ordering is not guaranteed so we're not checking `search` entirely
+    cy.location('search').should('contain', 'pageNo=1')
+    cy.location('search').should('contain', 'searchTerm=test')
+  })
 })
