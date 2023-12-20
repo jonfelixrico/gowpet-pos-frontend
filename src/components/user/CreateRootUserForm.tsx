5a895914a8bb3f5a9d74b843b502d60a97ebb0ac
Felix <jonfelixrico@gmail.com>
2023-12-21 00:04:20 +0800

    Create stub of the create form

---
+++ src/components/user/CreateRootUserForm.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { Credentials } from '@/types/login-types'
+import { Button, Flex, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Formik, Form, Field, FieldProps, FormikHelpers } from 'formik'
+
+interface ConfirmCredentials extends Credentials {
+  confirmPassword: string
+}
+
+export default function CreateRootUserForm({
+  onSubmit,
+}: {
+  onSubmit: (credentials: Credentials) => Promise<void>
+}) {
+  async function handleSubmit(
+    values: ConfirmCredentials,
+    actions: FormikHelpers<ConfirmCredentials>
+  ) {
+    try {
+      await onSubmit(values)
+    } finally {
+      actions.setSubmitting(false)
+    }
+  }
+
+  return (
+    <Formik
+      initialValues={
+        {
+          username: '',
+          password: '',
+          confirmPassword: '',
+        } as ConfirmCredentials
+      }
+      onSubmit={handleSubmit}
+    >
+      {(props) => (
+        <Form>
+          <Flex direction="column" alignItems="stretch" gap="2">
+            <Field name="username">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={!!form.errors.username && !!form.touched.username}
+                >
+                  <FormLabel>Username</FormLabel>
+                  <Input
+                    {...field}
+                    type="text"
+                    autoComplete="username"
+                    data-cy="username"
+                  />
+                </FormControl>
+              )}
+            </Field>
+
+            <Field name="password">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={!!form.errors.password && !!form.touched.password}
+                >
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    autoComplete="current-password"
+                    data-cy="password"
+                  />
+                </FormControl>
+              )}
+            </Field>
+
+            <Field name="confirmPassword">
+              {({ field, form }: FieldProps) => (
+                <FormControl
+                  isInvalid={!!form.errors.password && !!form.touched.password}
+                >
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    autoComplete="current-password"
+                    data-cy="password"
+                  />
+                </FormControl>
+              )}
+            </Field>
+
+            <Button
+              isLoading={props.isSubmitting}
+              type="submit"
+              data-cy="submit"
+              colorScheme="blue"
+            >
+              Log In
+            </Button>
+          </Flex>
+        </Form>
+      )}
+    </Formik>
+  )
+}
