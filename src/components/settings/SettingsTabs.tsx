e7d758ba85536f17eee29a3c2538cb208767569d
Felix <jonfelixrico@gmail.com>
2023-12-30 23:26:13 +0800

    Use dedicated component

--- src/components/settings/SettingsTabs.tsx
+++ src/components/settings/SettingsTabs.tsx
@@ -1,19 +1,62 @@
 'use client'
 
+import { Button } from '@chakra-ui/react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import RouteLinkButton from '@/components/common/RouteLinkButton'
+import { ReactNode, useMemo } from 'react'
+import { Else, If, Then } from 'react-if'
+
+function RouteLinkButton({
+  href,
+  isActive,
+  children,
+}: {
+  href: string
+  isActive: boolean
+  children: ReactNode
+}) {
+  return (
+    <Link href={href} prefetch={false}>
+      <If condition={isActive}>
+        <Then>
+          <Button width="full" colorScheme="blue">
+            {children}
+          </Button>
+        </Then>
+        <Else>
+          <Button width="full" color="black" bgColor="white" variant="outline">
+            {children}
+          </Button>
+        </Else>
+      </If>
+    </Link>
+  )
+}
+
+const SUBPATHS = ['billing', 'accounts']
+const SUBPATH_LABELS: Record<string, string> = {
+  billing: 'Billing',
+  accounts: 'Accounts',
+}
 
 export default function SettingsTabs() {
-  const path = usePathname()
+  const pathname = usePathname()
+  const currentSubpath = useMemo(() => {
+    const split = pathname.split('/')
+    return split[split.length - 1]
+  }, [pathname])
+
   return (
     <>
-      <RouteLinkButton
-        currentPath={path}
-        href="/settings/billing"
-        pattern={/\/billing$/}
-      >
-        Billing
-      </RouteLinkButton>
+      {SUBPATHS.map((subpath) => (
+        <RouteLinkButton
+          key={subpath}
+          href={`/settings/${subpath}`}
+          isActive={subpath === currentSubpath}
+        >
+          {SUBPATH_LABELS[subpath]}
+        </RouteLinkButton>
+      ))}
     </>
   )
 }
