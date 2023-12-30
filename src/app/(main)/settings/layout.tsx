a9ac204d9a1b8127c4bf37a5c3ce1346ddd54bc5
Felix <jonfelixrico@gmail.com>
2023-12-30 23:00:47 +0800

    Use the URL

--- src/app/(main)/settings/layout.tsx
+++ src/app/(main)/settings/layout.tsx
@@ -1,19 +1,14 @@
-import { Card, CardBody, Container, Flex, Text } from '@chakra-ui/react'
+import SettingsTabs from '@/components/settings/SettingsTabs'
+import { Box, Card, CardBody, Container, Flex, Text } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
 export default function SettingsLayout({ children }: { children: ReactNode }) {
   return (
-    <Container
-      maxW="container.md"
-      height="full"
-      as={Flex}
-      direction="column"
-      padding={2}
-      gap={2}
-    >
-      <Text fontSize="2xl" fontWeight="bold">
-        Billing Settings
-      </Text>
+    <Container maxW="container.lg" height="full" as={Flex} gap={2} padding={2}>
+      <Flex direction="column" gap={2}>
+        <SettingsTabs />
+      </Flex>
+
       <Card flex={1}>
         <CardBody>{children}</CardBody>
       </Card>
