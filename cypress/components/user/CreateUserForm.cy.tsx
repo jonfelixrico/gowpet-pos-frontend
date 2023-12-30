916c28b496f0b737abce9c69560ebcf3dd426f6b
Felix <jonfelixrico@gmail.com>
2023-12-30 15:00:09 +0800

    Add tests

---
+++ cypress/components/user/CreateUserForm.cy.tsx
@@ -0,0 +1,35 @@
+import CreateUserForm from '@/components/user/CreateUserForm'
+
+describe('CreateUserForm', () => {
+  it('prevents incomplete input', () => {
+    const onSubmit = cy.spy().as('submit')
+    cy.mount(<CreateUserForm onSubmit={onSubmit} />)
+
+    cy.dataCy('username').type('username')
+    cy.dataCy('submit').click()
+    cy.get('@submit').should('not.have.been.called')
+    cy.dataCy('username').clear()
+
+    cy.dataCy('password').type('password')
+    cy.dataCy('submit').click()
+    cy.get('@submit').should('not.have.been.called')
+    cy.dataCy('password').clear()
+
+    cy.dataCy('confirm-password').type('password')
+    cy.dataCy('submit').click()
+    cy.get('@submit').should('not.have.been.called')
+    cy.dataCy('confirm-password').clear()
+  })
+
+  it('block login for unconfirmed password', () => {
+    const onSubmit = cy.spy().as('submit')
+    cy.mount(<CreateUserForm onSubmit={onSubmit} />)
+
+    cy.dataCy('username').type('username')
+    cy.dataCy('password').type('password')
+    cy.dataCy('confirm-password').type('password')
+    cy.dataCy('submit').click()
+
+    cy.get('@submit').should('have.been.called')
+  })
+})
