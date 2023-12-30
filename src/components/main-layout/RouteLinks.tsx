e3349b94a83edc1d1c55b4950dd9122ca6b591ae
Felix <jonfelixrico@gmail.com>
2023-12-30 23:12:00 +0800

    Revert "Create RouteLinkButton"
    This reverts commit a7b9c6fb7042989b8741bf382f42254cd22d1774.

--- src/components/main-layout/RouteLinks.tsx
+++ src/components/main-layout/RouteLinks.tsx
@@ -1,40 +1,75 @@
 'use client'
 
-import { Flex } from '@chakra-ui/react'
+import { DataAttributes } from '@/types/DataAttributes'
+import { Url } from '@/types/Url'
+import { Button, Flex, Text } from '@chakra-ui/react'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import RouteLinkButton from '@/components/common/RouteLinkButton'
+import { ReactNode, useMemo } from 'react'
 
-export default function RouteLinkButtons() {
+function RouteLink({
+  href,
+  children,
+  pattern,
+  currentPath,
+  ...dataAttrs
+}: {
+  href: Url
+  children?: ReactNode
+  pattern: RegExp
+  currentPath: string
+} & DataAttributes) {
+  const isActive = useMemo(
+    () => pattern.test(currentPath),
+    [currentPath, pattern]
+  )
+
+  return (
+    <Link href={href} prefetch={false} {...dataAttrs}>
+      <Button
+        width="full"
+        variant="ghost"
+        colorScheme={isActive ? 'blue' : undefined}
+      >
+        <Text textAlign="left" width="full">
+          {children}
+        </Text>
+      </Button>
+    </Link>
+  )
+}
+
+export default function RouteLinks() {
   const pathname = usePathname()
 
   return (
     <Flex direction="column" gap={2}>
-      <RouteLinkButton
+      <RouteLink
         href="/catalog"
         pattern={/^\/catalog.*/}
         currentPath={pathname}
         data-route-link="catalog"
       >
         Catalog
-      </RouteLinkButton>
+      </RouteLink>
 
-      <RouteLinkButton
+      <RouteLink
         href="/billing"
         pattern={/^\/billing.*/}
         currentPath={pathname}
         data-route-link="billing"
       >
         Billing
-      </RouteLinkButton>
+      </RouteLink>
 
-      <RouteLinkButton
+      <RouteLink
         href="/settings"
         pattern={/^\/settings.*/}
         currentPath={pathname}
         data-route-link="settings"
       >
         Settings
-      </RouteLinkButton>
+      </RouteLink>
     </Flex>
   )
 }
