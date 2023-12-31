52f32b02eab6a075041c1bece79600c61aa33514
Felix <jonfelixrico@gmail.com>
2023-12-31 19:03:56 +0800

    Add some content

--- src/app/(main)/settings/accounts/page.tsx
+++ src/app/(main)/settings/accounts/page.tsx
@@ -1,3 +1,14 @@
+import { Box, Button, Flex } from '@chakra-ui/react'
+
 export default function AccountsSettingsPage() {
-  return <></>
+  return (
+    <Box height="full">
+      <Flex direction="column">
+        <Box height="100">TODO user list</Box>
+        <Flex justify="end">
+          <Button colorScheme="blue">Create User</Button>
+        </Flex>
+      </Flex>
+    </Box>
+  )
 }
