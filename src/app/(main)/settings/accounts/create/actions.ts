a61b5a8fc770702b1823679e4a8cc8d971b3184b
Felix <jonfelixrico@gmail.com>
2024-01-01 17:34:38 +0800

    Make the page a client page

---
+++ src/app/(main)/settings/accounts/create/actions.ts
@@ -0,0 +1,7 @@
+'use server'
+
+import { Credentials } from '@/types/login-types'
+
+export async function createUser(credentials: Credentials) {
+  // TODO do something
+}
--- src/app/(main)/settings/accounts/create/page.tsx
+++ src/app/(main)/settings/accounts/create/page.tsx
@@ -1,25 +1,47 @@
+'use client'
+
+import BaseCreateUserFormContainer from '@/components/user/create/BaseCreateUserFormContainer'
+import BaseCreateUserFormFields from '@/components/user/create/BaseCreateUserFormFields'
 import {
   Button,
+  Flex,
   ModalBody,
   ModalCloseButton,
   ModalFooter,
   ModalHeader,
 } from '@chakra-ui/react'
+import { Form } from 'formik'
+import { createUser } from './actions'
 
 export default function CreateAccountDialogPage() {
   return (
     <>
-      <ModalHeader>Create Account</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>Test</ModalBody>
+      <BaseCreateUserFormContainer onSubmit={createUser}>
+        {(props) => (
+          <Form>
+            <ModalHeader>Create Account</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody as={Flex} direction="column" gap={3}>
+              <BaseCreateUserFormFields />
+            </ModalBody>
 
-      <ModalFooter>
-        {/* TODO inject the close method here */}
-        <Button variant="ghost" mr={3}>
-          Cancel
-        </Button>
-        <Button colorScheme="blue">Create Account</Button>
-      </ModalFooter>
+            <ModalFooter>
+              {/* TODO inject the close method here */}
+              <Button variant="ghost" mr={3}>
+                Cancel
+              </Button>
+
+              <Button
+                colorScheme="blue"
+                type="submit"
+                isLoading={props.isSubmitting}
+              >
+                Create Account
+              </Button>
+            </ModalFooter>
+          </Form>
+        )}
+      </BaseCreateUserFormContainer>
     </>
   )
 }
