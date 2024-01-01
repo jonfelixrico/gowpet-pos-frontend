30fdd3c5218f0382495707c7c3946fb7d3403417
Felix <jonfelixrico@gmail.com>
2024-01-01 18:42:24 +0800

    Display the users

--- src/app/(main)/settings/accounts/@list/default.tsx
+++ src/app/(main)/settings/accounts/@list/default.tsx
@@ -1,7 +1,23 @@
-import { Box, Button, Center, Divider, Flex, Text } from '@chakra-ui/react'
+import UserTable from '@/components/settings/user/list/UserTable'
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import User from '@/types/User'
+import { Button, Center, Divider, Flex, Text } from '@chakra-ui/react'
 import Link from 'next/link'
+import { Else, If, Then } from 'react-if'
 
-export default function AccountsSettingsPage() {
+export default async function AccountsSettingsPage() {
+  /*
+    The BE endpoint supports pagination but since we don't expect a huge number of users, there is no need to implement
+    it in the FE yet.
+
+    TODO support pagination
+   */
+  const { data } = await apiFetchData<User[]>('/user?itemCount=999', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
   return (
     <Flex height="full" direction="column" gap={2}>
       <Flex justify="space-between" align="center">
@@ -15,9 +31,17 @@ export default function AccountsSettingsPage() {
 
       <Divider />
 
-      <Center flex={1}>
-        <Text fontSize="xl">No users to display</Text>
-      </Center>
+      <If condition={data.length}>
+        <Then>
+          <UserTable users={data} />
+        </Then>
+
+        <Else>
+          <Center flex={1}>
+            <Text fontSize="xl">No users to display</Text>
+          </Center>
+        </Else>
+      </If>
     </Flex>
   )
 }
