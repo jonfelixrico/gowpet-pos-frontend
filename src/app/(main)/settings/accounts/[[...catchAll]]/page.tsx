89ae5ece2bdfa1b43f9991fd1c3edbb237e33059
Felix <jonfelixrico@gmail.com>
2024-01-01 20:48:27 +0800

    Add comment

--- src/app/(main)/settings/accounts/[[...catchAll]]/page.tsx
+++ src/app/(main)/settings/accounts/[[...catchAll]]/page.tsx
@@ -4,6 +4,13 @@ import User from '@/types/User'
 import { Center, Text } from '@chakra-ui/react'
 import { Else, If, Then } from 'react-if'
 
+/*
+ * By using an optional catch-all route instead of a static route, we can show the same content for the list endpoint (/) and the
+ * create endpoint (/create) for the module.
+ * 
+ * Technically we can achieve the same effect by creating a static route for / and /create, but when switching between those two routes they will
+ * end up "refreshing". By using an optional catch-all route, we're telling Next that there's no need to "refresh" the content.
+ */
 export default async function UserListPage() {
   /*
     The BE endpoint supports pagination but since we don't expect a huge number of users, there is no need to implement
