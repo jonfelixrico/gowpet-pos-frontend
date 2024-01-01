e63b78076c0a3f319fcfac35108ec98abf3113ab
Felix <jonfelixrico@gmail.com>
2024-01-01 16:37:36 +0800

    Implement layout approach

---
+++ src/app/(main)/settings/accounts/create/layout.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
+import { ReactNode, useState } from 'react'
+
+export default function CreateAccountDialogLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  const router = useRouter()
+  const [isOpen, setIsOpen] = useState(true)
+
+  function onClose() {
+    setIsOpen(false)
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      onCloseComplete={() => router.back()}
+    >
+      <ModalOverlay />
+      {/* TODO provide close method here */}
+      <ModalContent>{children}</ModalContent>
+    </Modal>
+  )
+}
--- src/app/(main)/settings/accounts/create/page.tsx
+++ src/app/(main)/settings/accounts/create/page.tsx
@@ -2,44 +2,26 @@
 
 import {
   Button,
-  Modal,
   ModalBody,
   ModalCloseButton,
-  ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay,
 } from '@chakra-ui/react'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
 
 export default function CreateAccountDialogPage() {
-  const router = useRouter()
-  const [isOpen, setIsOpen] = useState(true)
-
-  function onClose() {
-    setIsOpen(false)
-  }
-
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      onCloseComplete={() => router.back()}
-    >
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Create Account</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>Test</ModalBody>
+    <>
+      <ModalHeader>Create Account</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>Test</ModalBody>
 
-        <ModalFooter>
-          <Button variant="ghost" mr={3} onClick={onClose}>
-            Cancel
-          </Button>
-          <Button colorScheme="blue">Create Account</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+      <ModalFooter>
+        {/* TODO inject the close method here */}
+        <Button variant="ghost" mr={3}>
+          Cancel
+        </Button>
+        <Button colorScheme="blue">Create Account</Button>
+      </ModalFooter>
+    </>
   )
 }
