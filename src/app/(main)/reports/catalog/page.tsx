ddec82935cd688d2b76d9f299fee3205f234fb5f
Felix <jonfelixrico@gmail.com>
2024-02-10 20:16:00 +0800

    Relocate

--- src/app/(main)/reports/catalog/page.tsx
+++ src/app/(main)/reports/catalog/page.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, Container } from '@chakra-ui/react'
 
-export default function ReportsPage() {
+export default function CatalogReportsPage() {
   return (
     <Container maxW="container.md" padding={2}>
       <Card>
