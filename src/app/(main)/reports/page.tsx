1addb40f9efda31aca5b2a61c574673cefc6ff83
Felix <jonfelixrico@gmail.com>
2024-02-10 20:09:11 +0800

    Create reports page

---
+++ src/app/(main)/reports/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@chakra-ui/react'
+
+export default function ReportsPage() {
+  return (
+    <Container maxW="container.md" padding={2}>
+      test
+    </Container>
+  )
+}
