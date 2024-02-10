294a657f0d00b2eee3d445aa99148c2bded89286
Felix <jonfelixrico@gmail.com>
2024-02-10 20:53:12 +0800

    Remove some types

--- src/types/catalog-report-typings.ts
+++ src/types/catalog-report-typings.ts
@@ -8,8 +8,3 @@ export interface CatalogReportItemReference {
   id: string
   name: string
 }
-
-export interface CatalogReportDto {
-  references: CatalogReportItemReference[]
-  entries: CatalogReportEntry[]
-}
