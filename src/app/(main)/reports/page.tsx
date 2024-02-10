2856dadd5aa0b9aa3f1a410a7905b2d06946695e
Felix <jonfelixrico@gmail.com>
2024-02-10 20:11:17 +0800

    Turn it into a card

--- src/app/(main)/reports/page.tsx
+++ src/app/(main)/reports/page.tsx
@@ -1,9 +1,11 @@
-import { Container } from '@chakra-ui/react'
+import { Card, CardBody, Container } from '@chakra-ui/react'
 
 export default function ReportsPage() {
   return (
     <Container maxW="container.md" padding={2}>
-      test
+      <Card>
+        <CardBody>test</CardBody>
+      </Card>
     </Container>
   )
 }
