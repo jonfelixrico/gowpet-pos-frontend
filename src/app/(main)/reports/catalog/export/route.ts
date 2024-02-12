7261d20717b3512c66520cc923d30502834b075c
Felix <jonfelixrico@gmail.com>
2024-02-12 22:41:14 +0800

    Fill in the route

--- src/app/(main)/reports/catalog/export/route.ts
+++ src/app/(main)/reports/catalog/export/route.ts
@@ -1,1 +1,28 @@
-export async function GET() {}
+import { apiFetchData } from '@/server-utils/resource-api-util'
+import {
+  CatalogReportEntry,
+  CatalogReportItemReference,
+} from '@/types/catalog-report-typings'
+import { HydratedEntry, hydrateEntries } from '@/utils/catalog-report-utils'
+import { NextRequest, NextResponse } from 'next/server'
+import { json2csv } from 'json-2-csv'
+
+export async function GET(request: NextRequest) {
+  const { data } = await apiFetchData<{
+    references: CatalogReportItemReference[]
+    entries: CatalogReportEntry[]
+  }>('/catalog/report')
+  const hydrated = hydrateEntries(data.entries, data.references)
+
+  const csv = json2csv(hydrated, {
+    keys: ['name', 'price', 'quantity'] as Array<keyof HydratedEntry>,
+  })
+
+  const filename = request.nextUrl.searchParams.get('filename')
+  return new NextResponse(csv, {
+    headers: {
+      'Content-Type': 'application/csv',
+      'Content-Disposition': `attachment; filename="${filename}"`,
+    },
+  })
+}
