86e046cef2a31816574d4f566078c18772530fab
Felix <jonfelixrico@gmail.com>
2024-02-12 22:16:55 +0800

    Create hydrator util

---
+++ src/utils/catalog-report-utils.ts
@@ -0,0 +1,23 @@
+import {
+  CatalogReportEntry,
+  CatalogReportItemReference,
+} from '@/types/catalog-report-typings'
+import { keyBy } from 'lodash'
+
+interface HydratedEntry extends CatalogReportEntry {
+  name: string
+}
+
+export function hydrateEntries(
+  entries: CatalogReportEntry[],
+  references: CatalogReportItemReference[]
+): HydratedEntry[] {
+  const indexedRefs = keyBy(references, (ref) => ref.id)
+
+  return entries.map((entry) => {
+    return {
+      ...entry,
+      name: indexedRefs[entry.catalogItemId]?.name,
+    }
+  })
+}
